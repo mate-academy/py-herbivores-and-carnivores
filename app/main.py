193e17657d5015c6123e7a9050f4e591c2423584
Kalyta Valerii <deptovka@gmail.com>
2022-01-25 14:33:04 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ class Animal:
         self.name = name
         self.health = health
         self.hidden = hidden
-        self.alive.append(self)
+        Animal.alive.append(self)
 
     def __repr__(self):
         return f"{{Name: {self.name}, " \
@@ -25,4 +25,4 @@ class Carnivore(Animal):
         if isinstance(other, Herbivore) and other.hidden is not True:
             other.health -= 50
         if other.health <= 0:
-            self.alive.remove(other)
+            Animal.alive.remove(other)
