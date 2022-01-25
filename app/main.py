230f31b2c30ef5c41d5b28a6e258f3936a8466c3
Alex Zhidkov <sashazudkov.@gmail.com>
2022-01-25 15:28:52 +0200

    add staticmethod

--- app/main.py
+++ app/main.py
@@ -15,7 +15,8 @@ class Animal:
 
 
 class Carnivore(Animal):
-    def bite(self, animal):
+    @staticmethod
+    def bite(animal):
         if isinstance(animal, Herbivore) and not animal.hidden:
             animal.health -= 50
             if animal.health == 0:
