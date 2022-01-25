dd235d2ec6a26f0ad441415e8c3de5e9d19c702c
NikitaZH1812 <zhukovna1812@gmail.com>
2022-01-25 22:39:00 +0200

    fix hide func

--- app/main.py
+++ app/main.py
@@ -15,10 +15,7 @@ class Animal:
 
 class Herbivore(Animal):
     def hide(self):
-        if self.hidden is False:
-            self.hidden = True
-        else:
-            self.hidden = False
+        self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
