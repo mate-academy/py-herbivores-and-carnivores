da6fc03911d914afb6f9dfa52db8367a19f720e0
Vasyl Smutok <23.smutok@gmail.com>
2022-05-25 10:10:51 +0300

    single quotes have been replaced by double quotes

--- app/main.py
+++ app/main.py
@@ -8,8 +8,8 @@ class Animal:
         self.__class__.alive.append(self)
 
     def __repr__(self):
-        return f'{{Name: {self.name}, Health: {self.health}, ' \
-               f'Hidden: {self.hidden}}}'
+        return f"{{Name: {self.name}, Health: {self.health}, " \
+               f"Hidden: {self.hidden}}}"
 
 
 class Herbivore(Animal):
