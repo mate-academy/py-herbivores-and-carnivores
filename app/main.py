c894b27316cb465c915c4b168178da14fe7531ba
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2022-05-26 19:02:34 +0300

    'A_few_fixes'

--- app/main.py
+++ app/main.py
@@ -16,21 +16,17 @@ class Animal:
 class Herbivore(Animal):
 
     def hide(self):
-        if self.hidden:
-            self.hidden = False
-        else:
-            self.hidden = True
+        self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
 
     def bite(self, other_animal):
         if isinstance(other_animal, Herbivore)\
-                and other_animal.hidden is False:
-            print('bited')
-            other_animal.health = other_animal.health - 50
+                and not other_animal.hidden:
+            other_animal.health -= 50
         if other_animal.health <= 0:
-            print(f"{other_animal.name} is dead")
+            f"{other_animal.name} is dead"
             Animal.alive.remove(other_animal)
         if other_animal.hidden:
-            print(f"{self.name} cannot bite hidden {other_animal.name}")
+            f"{self.name} cannot bite hidden {other_animal.name}"
