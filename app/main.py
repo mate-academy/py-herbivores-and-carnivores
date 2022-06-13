c93ee7a976fe465582ae26d342f3bec826d7d794
Artem Kazakov <artemkazakov947@gmail.com>
2022-06-13 16:29:48 +0300

    f was removed, changed hide status in one line, else in bite method was removed

--- app/main.py
+++ app/main.py
@@ -9,28 +9,23 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self):
-        return f'' \
-               f'{{'f'Name: {self.name}, ' \
-               f'Health: {self.health}, ' \
-               f'Hidden: {self.hidden}}}'
+        return  \
+            f'{{'f'Name: {self.name}, ' \
+            f'Health: {self.health}, ' \
+            f'Hidden: {self.hidden}}}'
 
 
 class Herbivore(Animal):
 
     def hide(self):
-        if self.hidden is False:
-            self.hidden = True
-        else:
-            self.hidden = False
+        self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
 
     @staticmethod
     def bite(other):
-        if isinstance(other, Carnivore) or other.hidden is True:
-            other.health = other.health
-        else:
+        if isinstance(other, Herbivore) and other.hidden is False:
             other.health -= 50
         if other.health <= 0:
             Animal.alive.remove(other)
