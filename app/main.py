e30b2d4bba37318c1cb4b48dd74221446606337b
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2022-06-29 12:57:28 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,34 @@
-# write your code here
+class Animal:
+
+    alive = []
+
+    def __init__(self, name: str, health: int = 100, hidden: bool = False):
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __repr__(self):
+        return "{Name: " + self.name + \
+               ", Health: " + str(self.health) + \
+               ", Hidden: " + str(self.hidden) + "}"
+
+
+class Herbivore(Animal):
+
+    def hide(self):
+        if not self.hidden:
+            self.hidden = True
+        else:
+            self.hidden = False
+
+
+class Carnivore(Animal):
+
+    @staticmethod
+    def bite(victim):
+        if not isinstance(victim, Carnivore):
+            if not victim.hidden:
+                victim.health -= 50
+            if victim.health <= 0:
+                Animal.alive.remove(victim)
