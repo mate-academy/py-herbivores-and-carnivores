130f18f0d025209c8324ebc3c6ee5eaf977cbccf
Anastasiia Shevchenko <anastasiia.sshevchenko@gmail.com>
2022-07-04 20:15:08 +0300

    MY_Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+class Animal:
+    def __init__(self, name: str, health=100, hidden=False):
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+
+class Herbivore(Animal):
+    def hide(self):
+        return super().__init__(self.name, self.health, hidden=True)
+
+class Сarnivore(Animal):
+    def bite(self):
+        return
