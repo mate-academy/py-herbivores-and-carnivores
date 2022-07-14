daea43511db227034d98b9bf2101c9e564fcf0a8
Slava Seredenko <vrungelsp@gmail.com>
2022-07-14 17:13:29 +0300

    Sem potov soydet poka odnu zadachu reshu

--- app/main.py
+++ app/main.py
@@ -1,1 +1,29 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name, health=100, hidden=False):
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self) if self.health > 0 else None
+
+    def __repr__(self):
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+
+
+class Herbivore(Animal):
+
+    def hide(self):
+        self.hidden = True if self.hidden is False else False
+
+
+class Carnivore(Animal):
+
+    @staticmethod
+    def bite(target):
+        if isinstance(target, Herbivore) and not target.hidden:
+            target.health -= 50
+            if target.health <= 0:
+                Animal.alive.remove(target)
+
+
