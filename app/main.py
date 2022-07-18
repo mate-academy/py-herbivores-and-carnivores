f79a3e531baf4e6c9637b059cf92b34937073b1a
Dima Povkh <dimapovkh@gmail.com>
2022-07-18 19:23:08 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,53 @@
-# write your code here
+
+class Animal:
+    alive = []
+
+    def __init__(self, name, health=100, hidden=False):
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __repr__(self):
+        return f"{{Name: {self.name}, Health: {self.health}, " \
+               f"Hidden: {self.hidden}}}"
+
+
+class Carnivore(Animal):
+
+    @staticmethod
+    def bite(target):
+        if isinstance(target, Herbivore) and not target.hidden:
+            target.health -= 50
+        if target.health <= 0:
+            Animal.alive.remove(target)
+
+
+class Herbivore(Animal):
+
+    def hide(self):
+        # if self.hidden is True:
+        #     self.hidden = False
+        # else:
+        #     self.hidden = True
+        self.hidden = not self.hidden
+
+# pantera = Carnivore("Bagira")
+# snake = Carnivore("Kaa")
+# print(Animal.alive)
+
+# lion = Carnivore("Lion King")
+# rabbit = Herbivore("Susan")
+# rabbit.health == 100
+# print(lion.bite(rabbit))
+# rabbit.health == 50
+# print(Animal.alive)
+
+
+# pantera.__dict__
+# snake.__dict__
+
+
+# lion = Carnivore("Simba")
+# len(Animal.alive[0]) == 1
+# isinstance(Animal.alive, Carnivore) is True
