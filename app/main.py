b7ac13196df7fb421c78f710e57f170047cd2461
AnDy <ansmbox@gmail.com>
2022-07-29 16:19:15 +0300

    refactor methd Animal.__repr__() (try 2)

--- app/main.py
+++ app/main.py
@@ -8,10 +8,10 @@ class Animal:
         self.alive.append(self)
 
     def __repr__(self):
-        name = f'Name: {self.name}'
-        health = f'Health: {self.health}'
-        hidden = f'Hidden: {self.hidden}'
-        return '{' + f'{name}, {health}, {hidden}' + '}'
+        name = f"Name: {self.name}"
+        health = f"Health: {self.health}"
+        hidden = f"Hidden: {self.hidden}"
+        return f"{{{name}, {health}, {hidden}}}"
 
 
 class Herbivore(Animal):
