a2239b3f77c95aa75974a047668bae1b2223517a
Egor Voloshyn <103663723+AriohBelskij@users.noreply.github.com>
2022-09-16 01:46:30 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,31 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100, hidden: bool = False):
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        self.alive.append(self)
+
+    def __repr__(self) -> str:
+        return f"{{Name: {self.name}, " \
+               f"Health: {self.health}, " \
+               f"Hidden: {self.hidden}}}"
+
+
+class Herbivore(Animal):
+    def hide(self):
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, victim: Animal):
+        if not isinstance(victim, Carnivore) \
+                and not victim.hidden:
+            victim.health -= 50
+        self.meat_grinder(victim)
+
+    def meat_grinder(self, meat: Animal):
+        if meat.health <= 0:
+            Animal.alive.pop(
+                Animal.alive.index(meat))
