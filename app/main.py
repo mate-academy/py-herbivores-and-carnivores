6500e9a767640ed7786aa7d92a07cc93541434a7
Andriy Makarenko <andriy@mate.academy>
2022-09-20 16:45:56 +0300

    Solution + int for health

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health=100, hidden: bool = False) -> None:
+    def __init__(self,
+                 name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
