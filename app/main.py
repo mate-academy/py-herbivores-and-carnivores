9fc6e61ad21269ee6348b93bc78e03f2b034ca17
Dima Ovcharenko <kukuku381@gmail.com>
2022-09-23 14:48:51 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ class Animal:
     def __init__(self,
                  name: str,
                  health: int = 100,
-                 hidden: bool = False):
+                 hidden: bool = False) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
