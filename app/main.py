d870928fc8ec8438cabc58aef97f81a1d11e6e9a
Artem Vykhovanets <80789163+AVyha@users.noreply.github.com>
2022-09-23 11:52:21 +0300

    add annotations

--- app/main.py
+++ app/main.py
@@ -1,18 +1,16 @@
 class Animal:
     alive = []
 
-    def __init__(self, name, health: int = 100):
+    def __init__(self, name: str, health: int = 100) -> None:
         self.name = name
         self.health = health
         self.hidden = False
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{" \
-               f"Name: {self.name}, " \
-               f"Health: {self.health}, " \
-               f"Hidden: {self.hidden}" \
-               f"}}"
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
