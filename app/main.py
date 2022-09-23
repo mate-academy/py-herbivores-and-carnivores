ea102588575411c519849e090569fbef692b85ef
Yevhenii Davydenko <akazhulich@gmail.com>
2022-09-23 21:30:52 +0300

    change func hide

--- app/main.py
+++ app/main.py
@@ -11,18 +11,14 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return ("{" + f"Name: {self.name}, "
-                      f"Health: {self.health}, "
-                      f"Hidden: {self.hidden}" + "}"
-                )
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
     def hide(self) -> None:
-        if self.hidden is True:
-            self.hidden = False
-        else:
-            self.hidden = True
+        self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
