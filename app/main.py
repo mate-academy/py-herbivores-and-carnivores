fe05367c823245b6f43c3ca1b23f7760a915f726
Kirill Drago <dragoswim7@gmail.com>
2022-09-23 11:57:39 +0300

    Added staticmethod and type annotation

--- app/main.py
+++ app/main.py
@@ -1,17 +1,18 @@
 class Animal:
     alive = []
 
-    def __init__(self, name, health=100, hidden=False):
+    def __init__(self, name: str, health: int = 100,
+                 hidden: bool = False) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
         Animal.alive.append(self)
 
-    def remove_from_alive(self):
+    def remove_from_alive(self) -> None:
         if self.health <= 0:
             Animal.alive.remove(self)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return (f"{{Name: {self.name}, "
                 f"Health: {self.health}, "
                 f"Hidden: {self.hidden}}}")
@@ -19,13 +20,14 @@ class Animal:
 
 class Herbivore(Animal):
 
-    def hide(self):
+    def hide(self) -> None:
         self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
 
-    def bite(self, animal):
+    @staticmethod
+    def bite(animal: Animal) -> None:
         if isinstance(animal, Herbivore) and not animal.hidden:
             animal.health -= 50
             animal.remove_from_alive()
