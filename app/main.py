514d7d61cb0c30089e55a82e9dbab4ea8242728e
Vitalii Shevchuk <shevch.nl@gmail.com>
2022-09-25 23:02:10 +0300

    fixed bool variable

--- app/main.py
+++ app/main.py
@@ -20,10 +20,7 @@ class Animal:
 
 class Herbivore(Animal):
     def hide(self):
-        if self.hidden:
-            self.hidden = False
-        else:
-            self.hidden = True
+        self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
