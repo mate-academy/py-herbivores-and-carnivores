7378a6c72c36803e053ed8a32a1c00edc21c40f7
Dima Ursatii <109001524+Menselface@users.noreply.github.com>
2022-09-26 15:27:50 +0300

    Probably  you mean like this

--- app/main.py
+++ app/main.py
@@ -13,8 +13,9 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{"f"Name: {self.name}, Health: {self.health}," \
-               f" Hidden: {self.hidden}}}"
+        return (f"{{"f"Name: {self.name}, "
+                f"Health: {self.health},"
+                f" Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
