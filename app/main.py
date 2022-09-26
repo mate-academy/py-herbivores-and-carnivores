b6d0d5352d1ff7bafed459ae30a8be41413f9a6e
Adrian Kuzhel <adrian.kujel@gmail.com>
2022-09-26 20:04:51 +0300

    Solution fixed

--- app/main.py
+++ app/main.py
@@ -10,8 +10,8 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        name, health, hidden = self.name, self.health, self.hidden
-        return f"{{Name: {name}, Health: {health}, Hidden: {hidden}}}"
+        return (f"{{Name: {self.name}, Health: {self.health},"
+                f" Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
