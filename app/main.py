149c2aed63623e758bd7a08699fcae10045f0952
IvanGLS <gladiolus0707@gmail.com>
2022-10-06 18:29:24 +0300

    'Fixed'

--- app/main.py
+++ app/main.py
@@ -1,28 +1,30 @@
 class Animal:
     alive = []
 
-    def __init__(self, name, health=100, hidden=False):
+    def __init__(self,
+                 name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
         Animal.alive.append(self)
 
-    def __repr__(self):
-        Name = "{Name: " + self.name
-        Health = ", Health: " + str(self.health)
-        Hidden = ", Hidden: " + str(self.hidden) + "}"
-        return Name + Health + Hidden
+    def __repr__(self) -> str:
+        return "{Name: " + f"{self.name}" + ", Health: " + \
+               f"{str(self.health)}" + ", Hidden: " + \
+               f"{str(self.hidden)}" + "}"
 
 
 class Herbivore(Animal):
 
-    def hide(self):
+    def hide(self) -> None:
         self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
     @staticmethod
-    def bite(carnivore_animal):
+    def bite(carnivore_animal: str) -> None:
         if isinstance(
                 carnivore_animal, Herbivore
         ) and carnivore_animal.hidden is not True:
