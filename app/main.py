83302a9b1d47c780da48455bf35d792193176bae
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-10-13 16:13:26 +0300

    create __repr__ animal

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 class Animal:
     live = []
 
-    def __init__(self, name, health = 100, hidden = False):
+    def __init__(self, name, health = 100, hidden = False) -> None:
         self.health = health
         self.hidden = hidden
         self.name = name
         self.__class__.live.append(self)
+    
+    def __repr__(self) -> str:
+        return f"{{Name: {self.name}, " \
+               f"Health: {self.health}, " \
+               f"Hidden: {self.hidden}}}"
 
 
 class Herbivore(Animal):
