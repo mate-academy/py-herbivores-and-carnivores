d7cb96b51aee4686cf98137542251e5cd523ee97
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-10-13 16:09:55 +0300

    create class

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+class Animal:
+    live = []
+
+    def __init__(self, name, health = 100, hidden = False):
+        self.health = health
+        self.hidden = hidden
+        self.name = name
+        self.__class__.live.append(self)
+
+
+class Herbivore(Animal):
+    
+    def hide(self):
+        self.hidden = True
+
+class Carnivore(Animal):
+
+    def bite(self, ):
