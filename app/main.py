323f8958e0e659673b24919575dcca9afbad4ad2
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-10-15 15:47:07 +0300

    flake8 change 2

--- app/main.py
+++ app/main.py
@@ -1,24 +1,24 @@
 class Animal:
     live = []
 
-    def __init__(self, name: str, health: int=100, hidden: bool=False) -> None:
+    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
         self.health = health
         self.hidden = hidden
         self.name = name
         self.__class__.live.append(self)
-    
+
     def __repr__(self) -> str:
         return f"{{Name: {self.name}, " \
                f"Health: {self.health}, " \
                f"Hidden: {self.hidden}}}"
 
 
-class Herbivore(Animal):    
+class Herbivore(Animal):
+
     def hide(self) -> None:
         self.hidden = not self.hidden
 
 class Carnivore(Animal):
-    
 
     @staticmethod
     def bite(herbivore: Herbivore) -> None:
