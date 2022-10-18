26b20e4280d0864601b40ed38da3bf9bd3659a77
Dede <specialized8393@gmail.com>
2022-10-18 22:32:11 +0300

    created 3 classes and test was 11 passed, 1 Error

--- app/main.py
+++ app/main.py
@@ -1,19 +1,25 @@
 class Animal:
-
     alive = []
-    alive.append()
-    def __init__(self, name: str, health: int = 100, ) -> None:
+
+    def __init__(self, name: str, health: int = 100) -> None:
         self.health = health
         self.name = name
         self.hidden = False
+        self.__class__.alive.append(self)
+
+    def __repr__(self):
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
 
     def __str__(self):
-
+        return f"{self.name}"
 
 
 class Herbivore(Animal):
     def hide(self):
-        self.hidden = True
+        if self.hidden:
+            self.hidden = False
+        else:
+            self.hidden = True
 
 
 class Carnivore(Animal):
@@ -22,3 +28,10 @@ class Carnivore(Animal):
         if isinstance(biter, Herbivore):
             if not biter.hidden:
                 biter.health -= 50
+                if biter.health <= 0:
+                    biter.alive.remove(biter)
+
+
+pantera = Carnivore("Bagira")
+snake = Carnivore("Kaa")
+print(Animal.alive)
