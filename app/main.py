1a43baf9b169f2936396d4c8b0289e3892012d7f
Dede <specialized8393@gmail.com>
2022-10-18 13:08:32 +0300

    created classes -> append animal and magic method print

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+class Animal:
+
+    alive = []
+    alive.append()
+    def __init__(self, name: str, health: int = 100, ) -> None:
+        self.health = health
+        self.name = name
+        self.hidden = False
+
+    def __str__(self):
+
+
+
+class Herbivore(Animal):
+    def hide(self):
+        self.hidden = True
+
+
+class Carnivore(Animal):
+    @staticmethod
+    def bite(biter: Herbivore) -> None:
+        if isinstance(biter, Herbivore):
+            if not biter.hidden:
+                biter.health -= 50
