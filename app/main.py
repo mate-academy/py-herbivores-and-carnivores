c787f7c01bec7f5b93e8ac62c7678949f96b2373
soobig666 <darkvader.ukr.net@gmail.com>
2022-10-26 19:01:21 +0300

    pre_solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,51 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str,  health: int = 100, hidden: bool = False) -> None:
+        self.health = health
+        self.name = name
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return f"{{Name: {self.name}, " \
+               f"Health: {self.health}, " \
+               f"Hidden: {self.hidden}}}"
+
+
+class Herbivore(Animal):
+
+    def hide(self):
+        if not self.hidden:
+            self.hidden = True
+        else:
+            self.hidden = False
+
+
+class Carnivore(Animal):
+    @staticmethod
+    def bite(animal: Animal) -> None:
+        animals = Animal.alive
+        if not isinstance(animal, Carnivore) and not animal.hidden:
+            animal.health -= 50
+            if animal.health <= 0:
+                animals.pop(animals.index(animal))
+
+
+# lion = Carnivore("Lion King")
+# rabbit = Herbivore("Susan")
+# rabbit.health == 100
+# lion.bite(rabbit)
+# print(rabbit.health == 50)
+#
+# rabbit.hide()
+# lion.bite(rabbit)
+# print(rabbit.health == 50)
+#
+# rabbit.hide()
+# lion.bite(rabbit)
+# print(rabbit.health == 0)
+
+pantera = Carnivore("Bagira")
+snake = Carnivore("Kaa")
+print(Animal.alive)
