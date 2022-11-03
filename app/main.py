9299dd7a9812e921c05c8c2aef6d04c82edc260f
Your Name <your_email@whatever.com>
2022-11-03 12:55:02 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+class Animal:
+  alive = []
+  def __init__(self, name: str = '', health: int = 100, hidden: bool = False):
+    self.health = health
+    self.name = name
+    self.hidden = hidden
+    Animal.alive.append(self)
+  
+  def __repr__(self):
+    return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+
+class Herbivore(Animal):
+  def hide(self):
+    self.hidden = not self.hidden
+
+class Carnivore(Animal):
+  def bite(self, herbivore: Animal):
+    if isinstance(herbivore, Herbivore) and herbivore.hidden is False:
+      herbivore.health -= 50
+      if herbivore.health <= 0:
+        Animal.alive.remove(herbivore)
