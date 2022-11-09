8a10f0d6c1afda65428673647dcebb6eab1c5c31
Oleg <88393037+ochernyi@users.noreply.github.com>
2022-11-09 11:39:30 +0200

    Method repr and bite have chenged

--- app/main.py
+++ app/main.py
@@ -9,9 +9,8 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        result = f"Name: {self.name}, Health: {self.health}, " \
-                 f"Hidden: {self.hidden}"
-        return "{" + result + "}"
+        return f"{{Name: {self.name}, Health: {self.health}, " \
+               f"Hidden: {self.hidden}}}"
 
 
 class Herbivore(Animal):
@@ -27,5 +26,5 @@ class Carnivore(Animal):
         if isinstance(herbivore, Herbivore):
             if not herbivore.hidden:
                 herbivore.health -= 50
-                if herbivore.health <= 0:
-                    Animal.alive.remove(herbivore)
+        if herbivore.health <= 0:
+            Animal.alive.remove(herbivore)
