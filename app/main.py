4438840939bb5dddff897f3f485c17c10a4df893
6aPa6aH <6apa6ah666@gmail.com>
2022-11-18 10:23:50 +0200

    code_reduction

--- app/main.py
+++ app/main.py
@@ -18,10 +18,7 @@ class Animal:
 class Herbivore(Animal):
 
     def hide(self) -> None:
-        if self.hidden is False:
-            self.hidden = True
-        else:
-            self.hidden = False
+        self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
