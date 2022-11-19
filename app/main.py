10e66d06682db8bcc017f55dd62fad1e69875b3d
Andrey Polyakov <polyakovv.andrey.01@gmail.com>
2022-11-20 01:33:12 +0200

    Herbivores and carnivores solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,45 @@
-# write your code here
+from __future__ import annotations
+from typing import Union
+
+
+class Animal:
+
+    alive = []
+
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        name = "{Name: " + str(self.name)
+        health = ", Health: " + str(self.health)
+        hidden = ", Hidden: " + str(self.hidden) + "}"
+        return name + health + hidden
+
+
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+        if self.hidden is False:
+            self.hidden = True
+        else:
+            self.hidden = False
+
+
+class Carnivore(Animal):
+
+    @staticmethod
+    def bite(animal: Union[Herbivore, Carnivore]) -> None:
+        if animal.hidden is False and isinstance(animal, Herbivore):
+            animal.health -= 50
+            if animal.health <= 0:
+                for index, beast in enumerate(Animal.alive):
+                    if beast.name == animal.name:
+                        del Animal.alive[index]
