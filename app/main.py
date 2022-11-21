fcb164f5e9974d30928144c19e4b7ad96ebb0429
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2022-11-21 20:25:34 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,38 +1,33 @@
-from __future__ import annotations
-
-
 class Animal:
     alive = []
-    health = 100
-    hidden = False
 
-    def __init__(self, name: str, *args) -> None:
+    def __init__(self,
+                 name: str,
+                 health: int = 100,
+                 hidden: bool = False,
+                 ) -> None:
         self.name = name
-        if len(args) == 1:
-            self.health = args[0]
-        elif len(args) == 2:
-            self.health = args[0]
-            self.hidden = args[1]
+        self.health = health
+        self.hidden = hidden
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return "{"\
-               + f"Name: {self.name}," \
-                 f" Health: {self.health}," \
-                 f" Hidden: {self.hidden}"\
-               + "}"
+        return f"{{Name: {self.name}, " \
+               f"Health: {self.health}, " \
+               f"Hidden: {self.hidden}}}"
+
+
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+        self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
 
     @staticmethod
-    def bite(victim: Herbivore) -> None:
-        if victim.__class__ is Herbivore and victim.hidden is False:
-            victim.health -= 50
-        if victim.health <= 0:
-            Animal.alive.remove(victim)
-
-
-class Herbivore(Animal):
-    def hide(self) -> None:
-        self.hidden = not self.hidden
+    def bite(prey: Herbivore) -> None:
+        if isinstance(prey, Herbivore) and prey.hidden is False:
+            prey.health -= 50
+        if prey.health <= 0:
+            Animal.alive.remove(prey)
