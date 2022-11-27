44fb04be3221f925070df6c898d4b227651ba991
Roman Kuryliak <romahakl@gmail.com>
2022-11-27 08:56:07 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,37 @@
-# write your code here
+from __future__ import annotations
+
+
+class Animal:
+    alive: list[Animal] = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.name: str = name
+        self.health: int = health
+        self.hidden: bool = False
+
+        Animal.alive.append(self)
+
+    @classmethod
+    def del_from_alive_if_0_hp(cls, herbivore: Herbivore) -> None:
+        if herbivore.health <= 0:
+            cls.alive.remove(herbivore)
+
+    def __repr__(self) -> str:
+        return "{{Name: {}, Health: {}, Hidden: {}}}".format(
+            self.name,
+            self.health,
+            self.hidden
+        )
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, herbivore: Herbivore) -> None:
+        if not herbivore.hidden and isinstance(herbivore, Herbivore):
+            herbivore.health -= 50
+
+        Animal.del_from_alive_if_0_hp(herbivore)
