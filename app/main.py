9959fbdbe7913d21350d002a3f17b5475d4c1f0a
Vitalii Panochko <vpapplic@gmail.com>
2022-12-06 23:33:07 +0200

    redone interpolation and if_else

--- app/main.py
+++ app/main.py
@@ -8,17 +8,13 @@ class Animal:
         self.alive.append(self)
 
     def __repr__(self) -> str:
-        return "{" + f"Name: {self.name}, " \
-                     f"Health: {self.health}, " \
-                     f"Hidden: {self.hidden}" + "}"
+        return "{Name: %s, Health: %s, Hidden: %s}" \
+               % (self.name, self.health, self.hidden)
 
 
 class Herbivore(Animal):
     def hide(self) -> None:
-        if not self.hidden:
-            self.hidden = True
-        else:
-            self.hidden = False
+        self.hidden = True if self.hidden is False else False
 
 
 class Carnivore(Animal):
