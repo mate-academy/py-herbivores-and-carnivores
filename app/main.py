4d8516c390d51425eb3173c77ebde6793299d4e8
Artur Leleiko <artur_98@ukr.net>
2022-12-31 16:38:34 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@ from __future__ import annotations
 
 
 class Animal:
-    hidden = False
     alive = []
 
     def __init__(self, name: str, health: int = 100) -> None:
         self.name = name
         self.health = health
         Animal.alive.append(self)
+        self.hidden = False
 
     def __repr__(self) -> str:
         return f"{{Name: {self.name}, "  \
@@ -19,15 +19,12 @@ class Animal:
 class Herbivore(Animal):
 
     def hide(self) -> None:
-        if self.hidden is False:
-            self.hidden = True
-        else:
-            self.hidden = False
+        self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
     def bite(self, animal: Herbivore | Carnivore) -> None:
         if isinstance(animal, Herbivore) and animal.hidden is False:
             animal.health -= 50
-            if animal.health <= 0:
-                Animal.alive.remove(animal)
+        if animal.health <= 0:
+            Animal.alive.remove(animal)
