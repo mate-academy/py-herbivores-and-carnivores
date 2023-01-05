9157a1c7bd56d62f4f6b4072385bb6d956a9a69b
Tymur Semelinskyi <t.semelinsky@gmail.com>
2023-01-05 16:39:29 +0100

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -8,16 +8,13 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return "{" + f"Name: {self.name}, Health: {self.health}," \
-                     f" Hidden: {self.hidden}" + "}"
+        return f"{{Name: {self.name}, Health: {self.health}, " \
+               f"Hidden: {self.hidden}}}"
 
 
 class Herbivore(Animal):
     def hide(self) -> None:
-        if self.hidden is False:
-            self.hidden = True
-        else:
-            self.hidden = False
+        self.hidden = bool(self.hidden - 1)
 
 
 class Carnivore(Animal):
