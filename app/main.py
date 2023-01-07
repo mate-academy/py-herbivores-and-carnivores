23320d797a42fa8f925b079dadaa9752f626e535
Tymur Semelinskyi <t.semelinsky@gmail.com>
2023-01-07 21:40:42 +0100

    'Solution4'

--- app/main.py
+++ app/main.py
@@ -8,20 +8,22 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}, Health: {self.health}, " \
-               f"Hidden: {self.hidden}}}"
+        name = self.name
+        health = self.health
+        hidden = self.hidden
+        return f"{{Name: {name}, Health: {health}, Hidden: {hidden}}}"
 
 
 class Herbivore(Animal):
     def hide(self) -> None:
-        self.hidden = True if self.hidden is False else False
+        self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
     @staticmethod
     def bite(other_animal: Animal) -> None:
-        if isinstance(other_animal, Herbivore) and \
-                other_animal.hidden is False:
+        if isinstance(other_animal,
+                      Herbivore) and other_animal.hidden is False:
             other_animal.health -= 50
         if other_animal.health <= 0:
             Animal.alive.remove(other_animal)
