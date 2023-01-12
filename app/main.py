8841c00d65cf6c49452be5ac09c9a41f9d587bba
Sashkoooo <o.denyseko@hotmail.com>
2023-01-12 15:39:35 +0200

    Develop

--- app/main.py
+++ app/main.py
@@ -1,1 +1,41 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.health = 100
+        self.hidden = False
+        self.alive.append(self)
+
+    # def alive(self):
+    #     return Animal.alive.__dict__
+
+
+class Herbivore(Animal):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    def bite(self) -> None:
+        if self.hidden is False:
+            self.health -= 50
+
+
+rabbit = Herbivore("Susan")
+lion = Carnivore("Lion King")
+print(rabbit.health)
+lion.bite(rabbit)
+print(rabbit.health)
+print(Animal.alive)
+print(rabbit in Animal.alive)
+
+# # rabbit.hide()
+# print(rabbit.hidden) #is True
+# class Carnivore(Animal):
