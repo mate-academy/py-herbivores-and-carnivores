bcbb584fbeacbb5b2766207062cc6621005ae402
Oleksandr <@gmail.com>
2023-01-12 23:34:06 +0200

    'Fixed2'

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ class Animal:
             self,
             name: str,
             health: int = 100,
-            hidden: bool = False) -> None:
+            hidden: bool = False
+    ) -> None:
 
         self.name = name
         self.health = health
@@ -13,9 +14,11 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return (f"{{Name: {self.name}, "
-                f"Health: {self.health}, "
-                f"Hidden: {self.hidden}}}")
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
 
 class Herbivore(Animal):
@@ -25,8 +28,10 @@ class Herbivore(Animal):
 
 class Carnivore(Animal):
     def bite(self, herbivore: Herbivore) -> None:
-        if isinstance(herbivore, Herbivore) \
-                and herbivore.hidden is False:
+        if (
+            isinstance(herbivore, Herbivore)
+            and herbivore.hidden is False
+        ):
             herbivore.health -= 50
         if herbivore.health <= 0:
             Animal.alive.pop()
