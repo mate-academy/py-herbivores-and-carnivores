36f0d9b3989b184c7a0500aba756cc2dcc45d62b
Illia Smyrnov <ilushasm@gmail.com>
2023-01-26 14:36:27 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,16 +2,14 @@ class Animal:
     alive = []
 
     def __init__(self, name: str, health: int = 100) -> None:
-        self.health = health
         self.name = name
+        self.health = health
         self.hidden = False
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        result = f"Name: {self.name}, " \
-                 f"Health: {self.health}, " \
-                 f"Hidden: {self.hidden}"
-        return "{" + str(result) + "}"
+        return f"{{Name: {self.name}, " \
+               f"Health: {self.health}, Hidden: {self.hidden}}}"
 
 
 class Herbivore(Animal):
