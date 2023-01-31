a8474c450c5345f893b68d912ee98c5cc008b6ff
Anastasiia <nastyashch1601@gmail.com>
2023-01-31 13:41:57 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -27,8 +27,7 @@ class Carnivore(Animal):
 
     @staticmethod
     def bite(animal: Animal) -> None:
-        if (isinstance(animal, Herbivore) is True
-           and animal.hidden is False):
+        if isinstance(animal, Herbivore) and not animal.hidden:
             animal.health -= 50
         if animal.health <= 0:
             Animal.alive.remove(animal)
