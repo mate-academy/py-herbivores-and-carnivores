26ceabb9bde630208d64db62b5d5bfa1650d798b
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-07 23:15:07 +0200

    2 commit

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ class Animal:
     def __init__(self, name: str,
                  health: int = 100,
                  hidden: bool = False) -> None:
+
         self.name = name
         self.health = health
         self.hidden = hidden
@@ -18,6 +19,7 @@ class Animal:
 class Carnivore(Animal):
     @staticmethod
     def bite(animal: Animal) -> None:
+
         if isinstance(animal, Herbivore) and not animal.hidden:
             animal.health -= 50
         if animal.health <= 0:
