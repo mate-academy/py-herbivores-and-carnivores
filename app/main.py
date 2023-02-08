f12b4b7ea73c6c329b61ca10736c56255c6f4d76
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-08 09:47:15 +0200

    add __dict__ to method repr instead of f-string

--- app/main.py
+++ app/main.py
@@ -12,13 +12,8 @@ class Animal:
         self.hidden = hidden
         Animal.alive.append(self)
 
-    def __str__(self) -> str:
-        return str(self.__dict__)
-
     def __repr__(self) -> str:
-        return str(f"{{Name: {self.name}, "
-                   f"Health: {self.health}, "
-                   f"Hidden: {self.hidden}}}")
+        return str(self.__dict__).title().replace("'", "")
 
 
 class Herbivore(Animal):
