77b448d7494dd86c60803462c24c9ec2a51efc68
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-09 13:34:16 +0200

    3 commit

--- app/main.py
+++ app/main.py
@@ -4,14 +4,12 @@ class Animal:
     def __init__(self, name: str,
                  health: int = 100,
                  hidden: bool = False) -> None:
-
         self.name = name
         self.health = health
         self.hidden = hidden
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-
         return f"{{Name: {self.name}, " \
                f"Health: {self.health}, " \
                f"Hidden: {self.hidden}}}"
@@ -30,7 +28,4 @@ class Carnivore(Animal):
 class Herbivore(Animal):
 
     def hide(self) -> None:
-        if self.hidden:
-            self.hidden = False
-        else:
-            self.hidden = True
+        self.hidden = not self.hidden
