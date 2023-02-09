f6bfc0efdda252c88d38ea21388958fb52745363
Max Bilokur <bilmaxim@gmail.com>
2023-02-09 23:17:54 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,35 @@
-# write your code here
+from __future__ import annotations
+
+
+class Animal:
+    alive = list()
+
+    def __init__(
+        self,
+        name: str,
+        health: int = 100,
+        hidden: bool = False
+    ) -> None:
+        self.health = health
+        self.name = name
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        me = f"Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}"
+        return "{" + me + "}"
+
+
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+        self.hidden = bool(int(self.hidden) - 1)
+
+
+class Carnivore(Animal):
+
+    def bite(self, prey: Animal) -> None:
+        if isinstance(prey, Herbivore) and not prey.hidden:
+            prey.health -= 50
+            if prey.health <= 0:
+                Animal.alive.remove(prey)
