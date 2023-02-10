2e730bd599f6f078b083e5ae8e35b6b69462f287
name <email@gmail.com>
2023-02-10 15:58:32 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,41 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return f"{{Name: {self.name}, " \
+               f"Health: {self.health}, " \
+               f"Hidden: {self.hidden}}}"
+
+    def set_health(self, health: int) -> None:
+        self.health = health
+
+    def is_hidden(self) -> bool:
+        return self.hidden
+
+    def is_dead(self) -> bool:
+        return self.health <= 0
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    @staticmethod
+    def bite(herbivore: Herbivore) -> None:
+        if isinstance(herbivore, Herbivore) and not herbivore.hidden:
+            herbivore.health -= 50
+            if herbivore.is_dead():
+                Animal.alive.remove(herbivore)
