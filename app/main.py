706e535ee289de4008f3f9102dbb596aa897d0bf
Yevhen Kopatko <cs.kopatko.evgen@gmail.com>
2023-02-10 16:02:58 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,37 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        brace1 = "{"
+        brace2 = "}"
+        return f"{brace1}Name: {self.name}, " \
+               f"Health: {self.health}, " \
+               f"Hidden: {self.hidden}{brace2}"
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        if self.hidden is False:
+            self.hidden = True
+        elif self.hidden is True:
+            self.hidden = False
+
+
+class Carnivore(Animal):
+    def bite(self, other: Herbivore) -> None:
+        if isinstance(other, Herbivore):
+            if other.hidden is False:
+                other.health -= 50
+        if other.health <= 0:
+            Animal.alive.remove(other)
