1da3754f58968bf02aeca54201df83a01f3d0c92
Bohdan Lazaryshyn <lazaryshyn1998@gmail.com>
2023-02-10 20:53:41 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -10,20 +10,15 @@ class Animal:
         self.hidden = hidden
         Animal.alive.append(self)
 
-    def __repr__(self):
-        return '{'f"Name: {self.name}, " \
+    def __repr__(self) -> str:
+        return "{"f"Name: {self.name}, " \
                f"Health: {self.health}, " \
-               f"Hidden: {self.hidden}"'}'
-
-    @classmethod
-    def if_death(cls, animal):
-        if animal.health <= 0:
-            Animal.alive.remove(animal)
+               f"Hidden: {self.hidden}""}"
 
 
 class Herbivore(Animal):
 
-    def hide(self):
+    def hide(self) -> None:
         if self.hidden is False:
             self.hidden = True
         else:
@@ -32,8 +27,8 @@ class Herbivore(Animal):
 
 class Carnivore(Animal):
 
-    def bite(self, animal: Animal):
+    def bite(self, animal: Animal) -> None:
         if isinstance(animal, Herbivore) and animal.hidden is False:
             animal.health -= 50
-
-        Animal.if_death(animal)
+        if animal.health <= 0:
+            Animal.alive.remove(animal)
