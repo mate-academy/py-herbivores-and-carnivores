230b6e2342b078e9b782ac9e94c16dbff7cbb589
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-10 15:59:45 +0200

    4 commit

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str,
+    def __init__(self,
+                 name: str,
                  health: int = 100,
                  hidden: bool = False) -> None:
         self.name = name
@@ -10,15 +11,14 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}, " \
-               f"Health: {self.health}, " \
-               f"Hidden: {self.hidden}}}"
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health},"
+                f" Hidden: {self.hidden}}}")
 
 
 class Carnivore(Animal):
     @staticmethod
     def bite(animal: Animal) -> None:
-
         if isinstance(animal, Herbivore) and not animal.hidden:
             animal.health -= 50
         if animal.health <= 0:
@@ -26,6 +26,5 @@ class Carnivore(Animal):
 
 
 class Herbivore(Animal):
-
     def hide(self) -> None:
         self.hidden = not self.hidden
