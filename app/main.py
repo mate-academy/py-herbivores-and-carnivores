6a65c233f35a6c0dfcabc0cef202c24cbeb30125
VladyslavCherkashyn <Sparrowq2@gmail.com>
2023-02-13 17:31:02 +0200

    ÑÑfixed line breaks

--- app/main.py
+++ app/main.py
@@ -17,11 +17,11 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{" \
-               f"Name: {self.name}," \
-               f" Health: {self.health}," \
-               f" Hidden: {self.hidden}" \
-               f"}}"
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
 
 class Herbivore(Animal):
