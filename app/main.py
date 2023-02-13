1ec1a16d9ef1ae9c09795989ff591b8166740c80
usrofgh <mailforgimail@gmail.com>
2023-02-13 19:22:15 +0200

    repr was changed and a couple methods were deleted

--- app/main.py
+++ app/main.py
@@ -13,18 +13,18 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}, " \
-               f"Health: {self.health}, " \
-               f"Hidden: {self.hidden}}}"
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
     def set_health(self, health: int) -> None:
         self.health = health
 
-    def is_hidden(self) -> bool:
-        return self.hidden
-
-    def is_dead(self) -> bool:
-        return self.health <= 0
+    def is_dead(self) -> None:
+        if self.health <= 0:
+            Animal.alive.remove(self)
 
 
 class Herbivore(Animal):
@@ -35,7 +35,6 @@ class Herbivore(Animal):
 class Carnivore(Animal):
     @staticmethod
     def bite(herbivore: Herbivore) -> None:
-        if isinstance(herbivore, Herbivore) and not herbivore.is_hidden():
+        if isinstance(herbivore, Herbivore) and not herbivore.hidden:
             herbivore.health -= 50
-            if herbivore.is_dead():
-                Animal.alive.remove(herbivore)
+            herbivore.is_dead()
