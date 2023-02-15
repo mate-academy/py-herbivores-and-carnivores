56ebec1ea4cb18a36494714bccabf5afdc811ffb
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-02-15 15:34:50 +0200

    removed slash from f-string

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str,
+    def __init__(self,
+                 name: str,
                  health: int = 100,
                  hidden: bool = False) -> None:
         self.health = health
@@ -10,9 +11,9 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}, " \
-               f"Health: {self.health}, " \
-               f"Hidden: {self.hidden}}}"
+        return (f"{{Name: {self.name}, "
+                f""f"Health: {self.health}, "
+                f""f"Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
