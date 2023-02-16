b4a3077c97c9c37ec2172deb9ad5ad58c8fa741f
Vitalii Shulepa <vitaliy.shulep@gmail.com>
2023-02-16 19:30:59 +0200

    Fixed line breakers

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ class Animal:
 
     alive = []
 
-    def __init__(self, name: str,
+    def __init__(self,
+                 name: str,
                  health: int = 100,
                  hidden: bool = False) -> None:
         self.name = name
@@ -11,9 +12,9 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}, " \
-               f"Health: {self.health}, " \
-               f"Hidden: {self.hidden}}}"
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
 
     @staticmethod
     def is_dead(lunch_name: str) -> None:
