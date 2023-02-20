ff9822f435b04d688bbfca680ca4503f2f420053
Igor Starodub <igorastar@gmail.com>
2023-02-20 18:56:44 +0200

    Solo1

--- app/main.py
+++ app/main.py
@@ -10,12 +10,12 @@ class Animal:
         self.health = health
         self.name = name
         self.hidden = hidden
-        self.__class__.alive.append(self)
+        self.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}, " \
-               f"Health: {self.health}, " \
-               f"Hidden: {self.hidden}}}"
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
