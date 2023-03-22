f0ab24a99ce429a6151791141b40b8aeff4c1911
Volodymyr <elohimwz@gmail.com>
2023-03-22 12:54:42 +0200

    'Solution#Herbivore'

--- app/main.py
+++ app/main.py
@@ -14,11 +14,9 @@ class Animal:
 
     def __repr__(self) -> str:
         return (
-            "{"
-            + f"Name: {self.name}, "
-            + f"Health: {self.health}, "
-            + f"Hidden: {self.hidden}"
-            + "}"
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
         )
 
 
