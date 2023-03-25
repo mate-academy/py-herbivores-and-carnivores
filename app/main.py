1234cc2d875a15479fb2047068d5d3c3b8f911d2
Dmytry Bondariev <bondarievmd@gmail.com>
2023-03-25 21:19:17 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ class Animal:
 
 class Herbivore(Animal):
     def hide(self) -> None:
-        self.hidden = True if self.hidden is not True else False
+        self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
