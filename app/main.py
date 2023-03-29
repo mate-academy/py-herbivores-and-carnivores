c2556df9a516fd77deaa12ee409e1ddc6d04f57a
Nikita <melniknikita2002@icloud.com>
2023-03-29 18:27:43 +0300

    Task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,35 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        self.alive.append(self)
+
+    def __repr__(self) -> str:
+        return f"{{Name: {self.name}," \
+               f" Health: {self.health}," \
+               f" Hidden: {self.hidden}}}"
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        if self.hidden:
+            self.hidden = False
+        else:
+            self.hidden = True
+
+
+class Carnivore(Animal):
+    def bite(self, animal: Animal) -> None:
+        if animal.hidden is False and isinstance(animal, Carnivore) is False:
+            animal.health -= 50
+
+        if animal.health <= 0:
+            self.alive.remove(animal)
