ee559bf28bc82cd8918e6923211153ac2fa01a5b
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-03-30 16:15:33 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
 class Animal:
 
     alive = []
-    def __init__(self, name: str, health: int = 100, hidden_attribute: bool = False) -> None:
+    
+    def __init__(self, name: str, health: int = 100,
+                 hidden_attribute: bool = False) -> None:
         self.health = health
         self.name = name
         self.hidden_attribute = hidden_attribute
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return print( f"{{Name: {self.name},"
-               f"Health: {self.health},"
-               f"Hidden_atribute: {self.hidden_attribute}}}")
+        return print(f"{{Name: {self.name},"
+                     f"Health: {self.health},"
+                     f"Hidden_atribute: {self.hidden_attribute}}}")
 
 
 class Herbivore(Animal):
@@ -23,7 +25,7 @@ class Herbivore(Animal):
 class Carnivore(Animal):
 
     @staticmethod
-    def bite(animal: Animal):
+    def bite(animal: Animal) -> None:
         if isinstance(animal, Herbivore) and animal.hidden_attribute is False:
             animal.health -= 50
         if animal.health <= 0:
