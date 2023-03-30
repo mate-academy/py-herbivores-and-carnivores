f5f3d7852a62716ca03e1dcc8e853e5b0054c03d
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-03-30 14:57:32 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,28 @@
-# write your code here
+class Animal:
+    def __init__(self, health: int = 100, name: str, hidden_attribute: bool = False) -> None:
+        self.health = health
+        self.name = name
+        self.hidden_attribute = hidden_attribute
+        Animal.alive.append(self)
+        
+    def __repr__(self) -> str:
+        reutrn f"{{Name: {self.name},"
+               f"Health: {self.health},"
+               f"Hidden_atribute: {self.hidden_attribute}}}"
+        
+        
+class Herbivor(Animal):
+    def hide(self) -> None:
+        self.hidden_attribute = not self.hidden_attribute
+
+        return self.hidden_attribute
+    
+    
+class Carnivore(Animal):
+    
+    @staticmethod
+    def bite(animal: Animal):
+        if isinstance(animal, Herbivore) and animal.hidden_attribute is False:
+            animal.health -= 50
+        if animal.health <= 0:
+            Animal.alive.remove(animal)
