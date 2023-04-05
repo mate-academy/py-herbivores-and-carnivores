c065b3c4936fe8dc3f93c7b8a16d3bdd468b3c39
V Borovets <vborovets0@gmail.com>
2023-04-05 11:34:31 +0300

    remove 'if' in append, rewrite 'bite' method

--- app/main.py
+++ app/main.py
@@ -9,32 +9,27 @@ class Animal:
         self.name = name
         self.health = health
         self.hidden = hidden
-        if self.health > 0:
-            Animal.alive.append(self)
+        Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}, " \
-               f"Health: {self.health}, " \
-               f"Hidden: {self.hidden}}}"
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
 
 class Herbivore(Animal):
 
     def hide(self) -> None:
-        if not self.hidden:
-            self.hidden = True
-        else:
-            self.hidden = False
+        self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
 
     def bite(self, bitten_animal: Animal) -> None:
-        if isinstance(bitten_animal, Herbivore):
-            if bitten_animal.hidden:
-                print(f"{self.name} cannot bite hidden {bitten_animal.name}")
-            else:
-                bitten_animal.health -= 50
+        if isinstance(bitten_animal, Herbivore) and not bitten_animal.hidden:
+            bitten_animal.health -= 50
 
         if bitten_animal.health <= 0:
             Animal.alive.remove(bitten_animal)
