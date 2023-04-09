01bd129ed29b165330b9f6a123b14ad57a0de203
Soloveyk1n <swain.volt@gmail.com>
2023-04-09 14:31:33 +0300

    last solution

--- app/main.py
+++ app/main.py
@@ -5,8 +5,7 @@ class Animal:
             self,
             name: str,
             health: int = 100,
-            hidden: bool = False
-            ) -> None:
+            hidden: bool = False) -> None:
         self.health = health
         self.name = name
         self.hidden = hidden
