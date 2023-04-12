dd16f7a8dfbacd6579fa31b5b1c4a5377f27ded2
Stanislav Bulanov <p99kab00@gmail.com>
2023-04-12 15:13:18 +0300

    Create initial implementation of the module

--- app/main.py
+++ app/main.py
@@ -1,1 +1,45 @@
-# write your code here
+from __future__ import annotations
+
+
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.name = name
+        self.hidden = False
+        self.health = health
+        Animal.alive.append(self)
+
+    def __repr__(self) -> dict:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
+
+    def take_damage(self, damage_rate: int) -> None:
+        if self.hidden:
+            return
+
+        self.health -= damage_rate
+
+        if self.health <= 0:
+            self.die()  # Rest in Peace, little one ='(
+
+    def die(self) -> None:
+        Animal.alive.remove(self)
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    damage_rate = 50
+
+    def bite(self, herbivore: Herbivore) -> None:
+        if not isinstance(herbivore, Herbivore):
+            return
+
+        herbivore.take_damage(self.damage_rate)
