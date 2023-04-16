0bd2ed784ea55b4b696e22197999c04b73e657e0
Denys Belei <denysbelei96>
2023-04-16 12:16:51 +0100

    "sol"

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+class Animal:
+    health = 100
+    hidden = False
+    alive = []
+
+    def __init__(self, *args) -> None:
+        self.name = args
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+        if self.hidden:
+            self.hidden = False
+        else:
+            self.hidden = True
+
+
+class Carnivore(Animal):
+
+    def bite(self, object: Herbivore) -> None:
+        if self.hidden is False:
+            object.health = object.health / 2
+
