d77141f8c8fb4c12623bdbc468c2ff7d24ea0617
Vadym <seredavadimsv@gmail.com>
2023-05-25 12:07:17 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -18,8 +18,11 @@ class Animal:
             Animal.alive.remove(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}, " \
-               f"Health: {self.health}, Hidden: {self.hidden}}}"
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
 
 class Herbivore(Animal):
