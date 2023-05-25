5fa48519839fed47d83aefef28a928783ce3592c
Oknametra <oknametra@gmail.com>
2023-05-25 15:24:18 +0200

    implemented class system for carnivores and herbivores

--- app/main.py
+++ app/main.py
@@ -1,1 +1,35 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        self.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name},"
+                f" Health: {self.health},"
+                f" Hidden: {self.hidden}}}")
+
+
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+        if not self.hidden:
+            self.hidden = True
+        else:
+            self.hidden = False
+
+
+class Carnivore(Animal):
+
+    @classmethod
+    def bite(cls, animal: Animal) -> None:
+        if not isinstance(animal, Carnivore):
+            if not animal.hidden:
+                animal.health -= 50
+                if animal.health <= 0:
+                    animal.alive.remove(animal)
