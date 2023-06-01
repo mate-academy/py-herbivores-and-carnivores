aca53aec7921282c72b87539ba297098c2e52675
YOUR_NAME <YOUR_EMAIL>
2023-06-01 17:40:09 +0300

    Animal (first commit)

--- app/main.py
+++ app/main.py
@@ -1,1 +1,32 @@
-# write your code here
+from __future__ import annotations
+
+
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
+
+
+class Carnivore(Animal):
+
+    @staticmethod
+    def bite(victim: Animal) -> None:
+        if not isinstance(victim, Carnivore) and not victim.hidden:
+            victim.health -= 50
+            if victim.health <= 0:
+                Animal.alive.remove(victim)
+
+
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+        self.hidden = not self.hidden
