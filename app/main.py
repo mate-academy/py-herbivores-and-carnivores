05eb8ca3c659883af9fbffaad6d2fb7d2eadf1f3
Volodymyr Komarnytskyi <volodimirkomarnickij28@gmail.com>
2023-06-01 19:21:11 +0300

    Changed some formatting on lines 7-8

--- app/main.py
+++ app/main.py
@@ -4,7 +4,9 @@ class Animal:
     def __init__(
             self, name: str,
             health: int = 100,
-            hidden: bool = False) -> None:
+            hidden: bool = False
+    ) -> None:
+
         self.health = health
         self.name = name
         self.hidden = hidden
