7449c97995c8daf8ea2b910b5a95c695c6163c30
Vlad <vladislav.zhyla@gmail.com>
2023-06-05 21:46:44 +0300

    Linebreaks and type annotation fixed. Condition simplified

--- app/main.py
+++ app/main.py
@@ -13,9 +13,9 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}, " \
-               f"Health: {self.health}, " \
-               f"Hidden: {self.hidden}}}"
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
@@ -25,8 +25,8 @@ class Herbivore(Animal):
 
 class Carnivore(Animal):
     @staticmethod
-    def bite(victim: "Herbivore") -> None:
-        if not isinstance(victim, Carnivore) and not victim.hidden:
+    def bite(victim: ["Herbivore", "Carnivore"]) -> None:
+        if isinstance(victim, Herbivore) and not victim.hidden:
             victim.health -= 50
             if victim.health <= 0:
                 Animal.alive.remove(victim)
