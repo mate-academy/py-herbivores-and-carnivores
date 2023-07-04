8e06b64c8bf9e1aa236cb2520d520a2fae71c240
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2023-07-04 18:00:50 +0200

    Corrected solution

--- app/main.py
+++ app/main.py
@@ -10,9 +10,8 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}, " \
-               f"Health: {self.health}, " \
-               f"Hidden: {self.hidden}}}"
+        return "{{Name: {}, Health: {}, Hidden: {}}}".format(
+            self.name, self.health, self.hidden)
 
 
 class Herbivore(Animal):
