0cd2bb893d1159636c16a8d53abe9040f01ab004
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-07-08 00:13:58 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,32 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health=100, hidden=False) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        self.alive.append(self)
+
+    def __repr__(self) -> str:
+        presentation_text = {
+            "Name": self.name,
+            "Health": self.health,
+            "Hidden": self.hidden
+        }
+        return str(presentation_text).replace("'", "")
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = True if self.hidden is False \
+            else False
+
+
+class Carnivore(Animal):
+    def bite(self, animal: Animal) -> None:
+        if animal.hidden is not True \
+                and isinstance(animal, Carnivore) is False:
+            animal.health -= 50
+            if animal.health <= 0:
+                animal.health = 0
+                self.alive.remove(animal)
