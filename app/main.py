6a5ea599c4743de28bebf56709a867b798a4c6fc
makukh <makukh@gmail.com>
2023-07-21 20:49:27 +0300

    save003

--- app/main.py
+++ app/main.py
@@ -10,24 +10,28 @@ class Animal:
 
     def print_info(self) -> None:
         for item in self.alive:
-            print(f"Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}")
+            print(
+                f"Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}"
+            )
 
 
 class Herbivore(Animal):
-    def __init__(self, name: str, health: int):
+    def __init__(self, name: str, health: int) -> None:
         super().__init__(self, name, health, hidden)
 
     @classmethod
-    def hide(cls):
+    def hide(cls) -> None:
         cls.hidden = True
 
 
 class Carnivore(Animal):
-    def __init__(self, name: str, health: int):
+    def __init__(self, name: str, health: int) -> None:
         super().__init__(self, name, health, hidden)
 
     @classmethod
-    def bite(cls, herbivore: Herbivore):
+    def bite(cls, herbivore: Herbivore) -> None:
         if Herbivore and Herbivore.hidden is False:
             herbivore.health -= 50
             if herbivore.health <= 0:
