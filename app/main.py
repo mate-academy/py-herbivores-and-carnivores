b0694f452a5b8b50f5f9f1bfc8df1060cfe6ef42
makukh <makukh@gmail.com>
2023-07-21 20:45:09 +0300

    save002

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ class Animal:
         self.name = name
         self.health = health
         self.hidden = False
+        self.alive.append(self)
+        # where to add instances to Animal.alive?
 
     def print_info(self) -> None:
         for item in self.alive:
@@ -15,16 +17,17 @@ class Herbivore(Animal):
     def __init__(self, name: str, health: int):
         super().__init__(self, name, health, hidden)
 
-    def hide(self):
-        self.hidden = True
+    @classmethod
+    def hide(cls):
+        cls.hidden = True
 
 
 class Carnivore(Animal):
     def __init__(self, name: str, health: int):
         super().__init__(self, name, health, hidden)
 
-    @staticmethod
-    def bite(self, herbivore: Herbivore):
+    @classmethod
+    def bite(cls, herbivore: Herbivore):
         if Herbivore and Herbivore.hidden is False:
             herbivore.health -= 50
             if herbivore.health <= 0:
