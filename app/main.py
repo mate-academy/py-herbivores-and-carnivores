a0fd02f6f9ee97a65ba08ca3b63f550d6d0de0e4
makukh <makukh@gmail.com>
2023-07-25 20:59:46 +0300

    Updated Solution

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ class Animal:
             health: int = 100,
             hidden: bool = False
     ) -> None:
-
         self.name = name
         self.health = health
         self.hidden = hidden
@@ -17,10 +16,11 @@ class Animal:
         Animal.alive.remove(self)
 
     def __repr__(self) -> str:
-        return f"{{" \
-               f"Name: {self.name}, " \
-               f"Health: {self.health}, " \
-               f"Hidden: {self.hidden}}}"
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
 
 class Herbivore(Animal):
