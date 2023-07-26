fcdd66c17e8db940a75ace876f2ccff3e664f00a
Oleksandr Plaksa <plaksa.sasha@gmail.com>
2023-07-26 17:19:49 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -13,9 +13,10 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return "{" + f"Name: {self.name}, " \
-                     f"Health: {self.health}, " \
-                     f"Hidden: {self.hidden}" + "}"
+        return (git 
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
