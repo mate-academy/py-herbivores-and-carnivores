27431604ec989c6f30b31be437bcd1e8f5f1e827
oreny <orenycdota@gmail.com>
2023-08-10 17:45:55 +0300

    Fixed flake8 errors

--- app/main.py
+++ app/main.py
@@ -1,17 +1,19 @@
-from typing import Callable
-
-
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
+    def __init__(self,
+                 name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
         self.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+        return f"{{Name: {self.name}," \
+               f" Health: {self.health}," \
+               f" Hidden: {self.hidden}}}"
 
 
 class Herbivore(Animal):
