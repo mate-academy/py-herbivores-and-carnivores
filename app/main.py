ce7a351c7911a246616757c2ed826ca50e19cbd5
oreny <orenycdota@gmail.com>
2023-08-10 17:43:16 +0300

    Solved Task. Created 2 classes and __repr__ to represent class as a string (fake dict).

--- app/main.py
+++ app/main.py
@@ -1,1 +1,34 @@
-# write your code here
+from typing import Callable
+
+
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        self.alive.append(self)
+
+    def __repr__(self) -> str:
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+
+    @staticmethod
+    def bite(bitten_animal: "Animal") -> None:
+        if isinstance(bitten_animal, Carnivore):
+            return
+        if bitten_animal.hidden:
+            return
+        bitten_animal.health -= 50
+        if bitten_animal in Animal.alive:
+            bitten_animal_index = Animal.alive.index(bitten_animal)
+            if bitten_animal.health <= 0:
+                Animal.alive.pop(bitten_animal_index)
