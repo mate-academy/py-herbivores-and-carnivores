37e8335bc6f7ce3dfc5de15334ca2b6ad0003907
Max Katkalov <makskatkalov@gmail.com>
2023-08-10 19:16:06 +0300

    solution implemented

--- app/main.py
+++ app/main.py
@@ -1,1 +1,48 @@
-# write your code here
+class Animal:
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    alive: list = []
+
+    def __repr__(self) -> str:
+        return f"{{Name: {self.name}," \
+               f" Health: {self.health}," \
+               f" Hidden: {self.hidden}}}"
+
+    @classmethod
+    def is_anyone_dead(
+        cls,
+    ) -> None:
+        for animal in cls.alive:
+            if animal.health <= 0:
+                del cls.alive[cls.alive.index(animal)]
+
+
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+        if self.hidden:
+            self.hidden = False
+        else:
+            self.hidden = True
+
+
+class Carnivore(Animal):
+
+    @staticmethod
+    def bite(
+            herbivore: Herbivore
+    ) -> None:
+        if isinstance(
+                herbivore, Herbivore
+        ) and herbivore.hidden is False:
+            herbivore.health -= 50
+        Animal.is_anyone_dead()
