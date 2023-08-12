e766d28e7f928972db3b3749161124fea6d9b9c4
Max Katkalov <makskatkalov@gmail.com>
2023-08-12 08:48:35 +0300

    blacked

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 class Animal:
     def __init__(
-            self,
-            name: str,
-            health: int = 100,
-            hidden: bool = False
+        self, name: str, health: int = 100, hidden: bool = False
     ) -> None:
         self.name = name
         self.health = health
@@ -13,33 +10,27 @@ class Animal:
     alive: list = []
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}," \
-               f" Health: {self.health}," \
-               f" Hidden: {self.hidden}}}"
+        return (
+            f"{{Name: {self.name},"
+            f" Health: {self.health},"
+            f" Hidden: {self.hidden}}}"
+        )
 
     @classmethod
-    def is_anyone_dead(
-        cls,
-    ) -> None:
+    def is_anyone_dead(cls) -> None:
         for animal in cls.alive:
             if animal.health <= 0:
                 del cls.alive[cls.alive.index(animal)]
 
 
 class Herbivore(Animal):
-
     def hide(self) -> None:
         self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
-
     @staticmethod
-    def bite(
-            herbivore: Herbivore
-    ) -> None:
-        if isinstance(
-                herbivore, Herbivore
-        ) and herbivore.hidden is False:
+    def bite(herbivore: Herbivore) -> None:
+        if isinstance(herbivore, Herbivore) and herbivore.hidden is False:
             herbivore.health -= 50
         Animal.is_anyone_dead()
