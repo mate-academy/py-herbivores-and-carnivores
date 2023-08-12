888e72a69dabeb64b7dcd7fc4a473921872c3db6
Oleh Chernous <chernous.oleh@gmail.com>
2023-08-12 19:01:47 +0200

    fixed Herbivore.hidden attribute, fixed return in __repr__ method, modified method bite to staticmethod, changed variable name herbivore_obj to herbivore

--- app/main.py
+++ app/main.py
@@ -13,19 +13,22 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}, " \
-               f"Health: {self.health}, " \
-               f"Hidden: {self.hidden}}}"
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
 
 class Herbivore(Animal):
     def hide(self) -> None:
-        self.hidden = False if self.hidden else True
+        self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
-    def bite(self, herbivore_obj: Herbivore) -> None:
-        if isinstance(herbivore_obj, Herbivore) and not herbivore_obj.hidden:
-            herbivore_obj.health -= 50
-            if herbivore_obj.health <= 0:
-                Animal.alive.remove(herbivore_obj)
+    @staticmethod
+    def bite(herbivore: Herbivore) -> None:
+        if isinstance(herbivore, Herbivore) and not herbivore.hidden:
+            herbivore.health -= 50
+            if herbivore.health <= 0:
+                Animal.alive.remove(herbivore)
