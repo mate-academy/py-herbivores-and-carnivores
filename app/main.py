c5b80a7c4616f6335320f9812c4bb1cf0971932b
Max Katkalov <makskatkalov@gmail.com>
2023-08-12 14:06:30 +0300

    remove implemented

--- app/main.py
+++ app/main.py
@@ -28,4 +28,4 @@ class Carnivore(Animal):
         if isinstance(herbivore, Herbivore) and herbivore.hidden is False:
             herbivore.health -= 50
             if herbivore.health <= 0:
-                del Animal.alive[Animal.alive.index(herbivore)]
+                Animal.alive.remove(herbivore)
