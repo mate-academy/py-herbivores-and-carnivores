e057856cf5f9e05eeea613c5f5099359dd34e759
Ruslan <rusipbox@gmail.com>
2023-08-13 12:44:32 +0300

    Test (py-herbivores-carnivores)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,20 @@
-from animal import Animal
+class Animal:
+    alive = []
+
+    def __init__(
+            self,
+            name: str, health: int = 100) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return f"{{Name: {self.name}, " \
+               f"Health: {self.health}, Hidden: {self.hidden}}}"
+
+    def die(self) -> None:
+        Animal.alive.remove(self)
 
 
 class Herbivore(Animal):
@@ -7,10 +23,9 @@ class Herbivore(Animal):
 
 
 class Carnivore(Animal):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-
     @staticmethod
-    def bite(herbivore: Herbivore) -> None:
-        if isinstance(herbivore, Herbivore) and not herbivore.hidden:
-            herbivore.get_damage(50)
+    def bite(target) -> None:
+        if isinstance(target, Herbivore) and not target.hidden:
+            target.health -= 50
+            if target.health <= 0:
+                target.die()
