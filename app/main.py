1c2c524609437231eb299167cd93bba8c2e4cc9c
Oleksandr Moroz <oleksandrmoroz2003@gmail.com>
2023-08-13 14:54:06 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,22 +1,24 @@
-from __future__ import annotations
-
-
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health: int = 100) -> None:
+    def __init__(
+        self,
+        name: str,
+        health: int = 100,
+        hidden: bool = False
+    ) -> None:
+
         self.health = health
         self.name = name
-        self.hidden = False
+        self.hidden = hidden
         self.alive.append(self)
 
-    def __str__(self) -> str:
-        return "{{Name: {}, Health: {}, Hidden: {}}}".format(
-            self.name, self.health, self.hidden
-        )
-
     def __repr__(self) -> str:
-        return self.__str__()
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
 
 class Herbivore(Animal):
@@ -25,8 +27,9 @@ class Herbivore(Animal):
 
 
 class Carnivore(Animal):
-    def bite(self, target: Herbivore) -> None:
+    @staticmethod
+    def bite(target: Herbivore) -> None:
         if isinstance(target, Herbivore) and not target.hidden:
             target.health -= 50
             if target.health <= 0:
-                super().alive.remove(target)
+                Animal.alive.remove(target)
