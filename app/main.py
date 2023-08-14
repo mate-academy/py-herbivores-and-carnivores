75382ba7c1b4f0661738fc306722b124cd5708d8
Eduard Habryd <eduard.boxing2001@gmail.com>
2023-08-14 09:07:02 +0300

    fixed repr return

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ class Animal:
     alive = []
 
     def __init__(
-            self,
-            name: str,
-            health: int = 100,
-            hidden: bool = False
+        self,
+        name: str,
+        health: int = 100,
+        hidden: bool = False
     ) -> None:
         self.name = name
         self.health = health
@@ -13,26 +13,20 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return (
-            "{"
-            + f"Name: {self.name}, "
-            + f"Health: {self.health}, "
-            + f"Hidden: {self.hidden}"
-            + "}"
-        )
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
 
     def remove_from_alive(self) -> None:
         Animal.alive.remove(self)
 
 
 class Herbivore(Animal):
-
     def hide(self) -> None:
         self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
-
     @staticmethod
     def bite(herbivore: Herbivore) -> None:
         if isinstance(herbivore, Herbivore) and not herbivore.hidden:
