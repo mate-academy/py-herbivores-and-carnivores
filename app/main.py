8eb10859d10b7de5444b1b0924d9474e375c3a62
Nikita Plotnytskyi <Nikita.plotnitckiy@gmail.com>
2023-08-18 02:38:47 +0300

    Herbivores and carnivores

--- app/main.py
+++ app/main.py
@@ -1,1 +1,38 @@
-# write your code here
+from typing import Union
+
+
+class Animal:
+    alive = []
+
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        self.alive.append(self)
+
+    def __repr__(self) -> str:
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        if self.hidden:
+            self.hidden = False
+        else:
+            self.hidden = True
+
+
+class Carnivore(Animal):
+    def bite(self, other: Union["Herbivore", "Carnivore"]) -> None:
+        if not other.hidden and isinstance(other, Herbivore):
+            other.health -= 50
+        if other.health <= 0:
+            Animal.alive.remove(other)
+
+
+
