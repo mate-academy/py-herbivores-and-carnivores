975b6fd6ce9460c437db17fcaf38ed51d74dcd6f
Poplinskiy Dmytro <super.shepard2@gmail.com>
2023-09-04 20:48:36 +0300

    change __repr__, type annotation, bite to static method

--- app/main.py
+++ app/main.py
@@ -17,11 +17,11 @@ class Animal:
         self.alive.append(self)
 
     def __repr__(self) -> str:
-        return ", ".join([
+        return (
             f"{{Name: {self.name}, "
             f"Health: {self.health}, "
             f"Hidden: {self.hidden}}}"
-        ])
+        )
 
 
 class Herbivore(Animal):
@@ -30,8 +30,9 @@ class Herbivore(Animal):
 
 
 class Carnivore(Animal):
-    def bite(self, beast: Animal) -> None:
+    @staticmethod
+    def bite(beast: Herbivore) -> None:
         if not beast.hidden and isinstance(beast, Herbivore):
             beast.health = beast.health - 50
             if beast.health <= 0:
-                super().alive.remove(beast)
+                Animal.alive.remove(beast)
