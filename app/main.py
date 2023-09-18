c10e78e8e7e8d84764591385d30175928275b8e3
Andrey Britov <abritov82@gmail.com>
2023-09-18 14:08:57 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,47 @@
-# write your code here
+class Animal:
+
+    alive = []
+
+    def __init__(self,
+                 name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+        # Animal.alive.append(self.__dict__)
+        print(Animal.alive, len(Animal.alive))
+
+    # def __str__(self):
+    #     res = []
+    #     for _ in self.alive:
+    #         res.append(self.__dict__)
+    #     Animal.alive = res
+    #     return Animal.alive
+
+
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+        if self.hidden is False:
+            self.hidden = True
+        else:
+            self.hidden = False
+
+
+class Carnivore(Animal):
+
+    def bite(self, other: object):
+        if other.hidden == False and type(other) != Carnivore:
+            other.health -= 50
+        if other.health <= 0:
+            Animal.alive.remove(other)
+        # for i in Animal.alive:
+        #     if i["health"] <= 0:
+        #         Animal.alive.remove(i)
+
+
+pantera = Carnivore("Bagira")
+snake = Carnivore("Kaa")
+# print(Animal.alive)
