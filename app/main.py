30d4e7a7c9e575077fc43a799bcf4fb8180e06ba
Olena Lisova <lisovausa@gmail.com>
2023-09-28 17:06:09 -0400

    Done py_herbivores_and_carnivors

--- app/main.py
+++ app/main.py
@@ -1,1 +1,36 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return "{Name:" \
+            + f" {self.name}, Health: {self.health}, Hidden: {self.hidden}"\
+            + "}"
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        if self.hidden:
+            self.hidden = False
+        else:
+            self.hidden = True
+
+
+class Carnivore(Animal):
+    def bite(self, animal: Herbivore) -> None:
+        if (animal in Animal.alive
+                and isinstance(animal, Herbivore)
+                and not animal.hidden):
+            animal.health -= 50
+            if animal.health <= 0:
+                Animal.alive.remove(animal)
