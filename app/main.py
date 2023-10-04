e8222263ca4a6c5d3971db75d6a307f5c7763ba4
unknown <temakot254@gmail.com>
2023-10-04 10:44:24 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ class Herbivore(Animal):
 
 
 class Carnivore(Animal):
-    def bite(self, herbivore: str) -> None:
+    def bite(self, herbivore: str) -> list:
         if isinstance(herbivore, Herbivore) and not herbivore.hidden:
             herbivore.health -= 50
             if herbivore.health <= 0:
