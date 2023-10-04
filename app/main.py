2b612b4637d2108782e50aef8fc11bd140746cb5
Юрий <you@exampleabc1234567892777@gmail.com>
2023-10-04 19:03:24 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,53 @@
-# write your code here
+from typing import List, Type, TypeVar
+
+T = TypeVar("T", bound="Animal")
+
+
+class Animal:
+
+    alive = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.health = health
+        self.name = name
+        self.hidden = False
+
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (
+            f"{{"
+            f"Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}"
+            f"}}"
+        )
+
+    @staticmethod
+    def check_health(animal: "Animal") -> None:
+        if animal.health <= 0:
+            Animal.alive.remove(animal)
+
+    @classmethod
+    def get_alive(cls: Type[T]) -> List[T]:
+        return cls.alive
+
+
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+
+    def bite(self, animal: Animal) -> None:
+        if not isinstance(animal, Herbivore):
+            return
+
+        if animal.hidden:
+            return
+
+        animal.health -= 50
+
+        Animal.check_health(animal)
