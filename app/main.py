0d81faf7a167acb9eefe2c4e561669f6a5e9568e
Valeriia Danyliuk <bladeryz@gmail.com>
2023-10-08 14:48:14 +0300

    Removed line breakers

--- app/main.py
+++ app/main.py
@@ -7,16 +7,15 @@ class Animal:
             health: int = 100,
             hidden: bool = False
     ) -> None:
-
         self.health = health
         self.name = name
         self.hidden = hidden
         self.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}," \
-               f" Health: {self.health}," \
-               f" Hidden: {self.hidden}}}"
+        return (f"{{Name: {self.name},"
+                f" Health: {self.health},"
+                f" Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
