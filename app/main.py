6816e02f3d4bad4dcb655daec9b75f84cef1fa54
Denys <den.prislipskyi@gmail.com>
2023-10-09 13:22:21 +0200

    fix test

--- app/main.py
+++ app/main.py
@@ -10,9 +10,11 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}," \
-               f"Health: {self.health}," \
-               f"Hidden: {self.hidden}}}"
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
     def remove_animal(self) -> None:
         if self in Animal.alive:
