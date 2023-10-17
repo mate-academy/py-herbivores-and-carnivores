a71a2df051c11e1b46f58ec53825529eb440d39d
V_core <bitard1990vn@gmail.com>
2023-10-17 23:07:04 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from typing import Union
+from __future__ import annotations
+from typing import Type
 
 
 class Animal:
@@ -8,15 +9,16 @@ class Animal:
             self,
             name: str,
             health: int = 100,
-            hidden: bool = False) -> None:
+            hidden: bool = False
+    ) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
         self.alive.append(self)
 
     @classmethod
-    def remove_dead_animal(cls) -> None:
-        cls.alive = [animal for animal in cls.alive if animal.health > 0]
+    def remove_dead_animal(cls, animal: Type[Herbivore]) -> None:
+        cls.alive.remove(animal)
 
     def __repr__(self) -> str:
         return (f"{{Name: {self.name}, "
@@ -24,15 +26,16 @@ class Animal:
                 f"Hidden: {self.hidden}}}")
 
 
-class Carnivore(Animal):
-
-    def bite(self, target: Union) -> None:
-        target.health -= 50 \
-            if (isinstance(target, Herbivore) and not target.hidden) else 0
-        Animal.remove_dead_animal()
-
-
 class Herbivore(Animal):
 
     def hide(self) -> None:
         self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+
+    def bite(self, target: Type[Herbivore]) -> None:
+        if (isinstance(target, Herbivore)) and (not target.hidden):
+            target.health -= 50
+            if target.health <= 0:
+                Animal.remove_dead_animal(target)
