37e12d6e3c8e63773d8c3e97e580d03c274d2a4b
Yuliia Dudych <juliadudych@gmail.com>
2023-10-22 17:10:21 +0300

    Create Animal class

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
+
+        self.health = health
+        self.name = name
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def is_dead(self) -> None:
+        if self.health <= 0:
+            Animal.alive.remove(self)
+
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
