ca5da8e111226a8efe00bc8426ac56a534d99c9b
AbsoluteProgramist <108784501+maksimpro001@users.noreply.github.com>
2023-10-24 14:42:33 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -17,9 +17,9 @@ class Animal:
         super().__setattr__(attr, value)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}, " \
-               f"Health: {self.health}, " \
-               f"Hidden: {self.hidden}}}"
+        return (f"{{Name: {self.name}, " +
+               f"Health: {self.health}, " +
+               f"Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
