2cde3f43297d130a2ffd1385d142a6b18228abc2
Vladyslav Bazhyn <147983482+VladyslavBazhyn@users.noreply.github.com>
2023-11-10 13:39:59 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -14,8 +14,8 @@ class Animal:
 
     def __repr__(self) -> str:
         
-        return (f"{{Name: {self.name}, 
-                    "f"Health: {self.health}, 
+        return (f"{{Name: {self.name}, ()
+                    "f"Health: {self.health},
                     "f"Hidden: {self.hidden}}}")
 
 
