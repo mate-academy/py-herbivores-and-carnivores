119a3d3621bcc4bac0b44ed01336debd00149079
xboris <boris.tikhonov.21@gmail.com>
2023-11-10 20:54:16 +0200

    fixed attribute

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ class Animal:
     def __init__(self,
                  name:
                  str, health:
-                 int = 100, hidden:
-                 bool = False) -> None:
+                 int = 100,
+                 hidden: bool = False) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
