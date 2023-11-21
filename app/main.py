fba8155e2f958d9802d42ce6ff274b602d521f2a
Oleksandr Asauliak <varmen67@gmail.com>
2023-11-21 16:50:39 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,29 +1,29 @@
 lion = [
     {
-        'Name': "Simba",
-        'Health': 100,
-        'Hidden': False
+        "Name": "Simba",
+        "Health": 100,
+        "Hidden": False
     },
 
     {
-        'Name': "King Lion",
-        'Health': 100,
-        'Hidden':  False,
+        "Name": "King Lion",
+        "Health": 100,
+        "Hidden": False,
     }
 ]
 rabbit = [
     {
-        'Name': "Susan",
-        'Health': 100,
-        'Hidden': False
+        "Name": "Susan",
+        "Health": 100,
+        "Hidden": False
     }
 ]
 
 pantera = [
     {
-        'Name': "Bagire",
-        'Health': 100,
-        'Hidden': False
+        "Name": "Bagire",
+        "Health": 100,
+        "Hidden": False
     }
 ]
 
@@ -31,26 +31,34 @@ pantera = [
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
         self.health = health
         self.name = name
         self.hidden = hidden
         Animal.alive.append(self)
 
+    def __repr__(self) -> str:
+        return f"{{" \
+               f"Name: {self.name}," \
+               f" Health: {self.health}," \
+               f" Hidden: {self.hidden}" \
+               f"}}"
+
 
 class Herbivore(Animal):
-    def hide(self):
-        if self.health == 50:
-            self.hidden = not self.hidden
+    def hide(self) -> None:
+        self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
-    def bite(self, rabbit):
-        if rabbit.hidden:
-            rabbit.health = 100
-        else:
-            rabbit.health -= 50
+    def bite(self, rabbit: Herbivore) -> None:
+        if isinstance(rabbit, Carnivore) or rabbit.hidden:
+            return
+        rabbit.health -= 50
         if rabbit.health <= 0:
             Animal.alive.remove(rabbit)
-
-
