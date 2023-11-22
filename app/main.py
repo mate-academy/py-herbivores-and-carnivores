31cb93acec1869e0a9aca826edae88345719b6e5
Damodar <alaso.se@gmail.com>
2023-11-22 15:57:33 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,31 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str,
+                 health: int = 100,
+                 hidden: bool = False
+                 ) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        self.alive.append(self)
+
+    def __repr__(self) -> str:
+        first_upper = {
+            key.title(): value for key, value in self.__dict__.items()
+        }
+        first_upper = str(first_upper).replace("'", "")
+        return f"{first_upper}"
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, other: Herbivore) -> None:
+        if isinstance(other, Herbivore) and other.hidden is False:
+            other.health -= 50
+            if other.health <= 0:
+                self.alive.remove(other)
