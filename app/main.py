ae2239ce19fb6e4b4f6c63502fb1f21fe6f1859c
Vladyslav Moiseienko <moiseenkov651@gmail.com>
2023-11-24 15:08:09 +0200

    Corrected def bite

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ class Herbivore(Animal):
 
 class Carnivore(Animal):
     @staticmethod
-    def bite(other: "Herbivore") -> None:
+    def bite(other: Herbivore) -> None:
         if isinstance(other, Herbivore) and not other.hidden:
             other.health -= 50
             if other.health <= 0:
