9cc191bfcca4c3f98f148fd97b67351d0dfe7ae1
Zoresław Dmytryszyn <zoreslav.dmytryshyn@gmail.com>
2023-11-27 17:18:59 +0100

    used parentheses instead of slashes

--- app/main.py
+++ app/main.py
@@ -13,11 +13,13 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{" \
-               f"Name: {self.name}, " \
-               f"Health: {self.health}, " \
-               f"Hidden: {self.hidden}" \
-               f"}}"
+        return (
+            f"{{"
+            f"Name: {self.name},"
+            f" Health: {self.health},"
+            f" Hidden: {self.hidden}"
+            f"}}"
+        )
 
 
 class Herbivore(Animal):
