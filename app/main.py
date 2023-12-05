977b6e903f0cc70a7790f2436cb68fdeb443e6cf
Dmytro Gertsovenko <gertsovenko@gmail.com>
2023-12-05 03:22:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,34 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return str(
+            {"Name": self.name,
+             "Health": self.health,
+             "Hidden": self.hidden
+             }
+        ).replace("'", "")
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        if self.hidden:
+            self.hidden = False
+        else:
+            self.hidden = True
+
+
+class Carnivore(Animal):
+    @staticmethod
+    def bite(animal: Animal) -> None:
+        if isinstance(animal, Herbivore):
+            if not animal.hidden:
+                animal.health -= 50
+                if animal.health <= 0:
+                    Animal.alive.remove(animal)
