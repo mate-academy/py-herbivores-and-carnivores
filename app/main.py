f067ef65726d31d805cd855b13ae26f36001c4c7
Yevhen Kunieiev <barelosfreda@gmail.com>
2023-12-12 12:11:01 +0200

    py-herbivores-and-carnivores 1.2 (After QnA)

--- app/main.py
+++ app/main.py
@@ -9,19 +9,14 @@ class Animal:
         self.name = name
         self.health = health
         self.hidden = hidden
-        self.adding_to_alive()
-
-    def adding_to_alive(self) -> None:
         self.alive.append(self)
 
-    @classmethod
-    def remove_animal(cls, herbivore_unit: object) -> None:
-        cls.alive.remove(herbivore_unit)
-
     def __repr__(self) -> str:
-        prep_msg = "Name: {0}, Health: {1}," \
-                   " Hidden: {2}".format(self.name, self.health, self.hidden)
-        return f"{{{prep_msg}}}"
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
 
 class Herbivore(Animal):
@@ -36,4 +31,4 @@ class Carnivore(Animal):
         if isinstance(herbivore, Herbivore) and not herbivore.hidden:
             herbivore.health = herbivore.health - 50
             if herbivore.health <= 0:
-                Animal.remove_animal(herbivore)
+                Animal.alive.remove(herbivore)
