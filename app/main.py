e322c5bc1829d68fe22131753d1b7551c84cc91d
Julia Okhrymenko <Ohrimenko15@gmail.com>
2023-12-20 21:45:32 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -14,9 +14,11 @@ class Animal:
             Animal.alive.remove(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}," \
-               f" Health: {self.health}," \
-               f" Hidden: {self.hidden}}}"
+        return (
+            f"{{Name: {self.name},"
+            f" Health: {self.health},"
+            f" Hidden: {self.hidden}}}"
+        )
 
 
 class Herbivore(Animal):
@@ -26,6 +28,6 @@ class Herbivore(Animal):
 
 class Carnivore(Animal):
     def bite(self, herbivore: Animal) -> None:
-        if not isinstance(herbivore, Carnivore) and not herbivore.hidden:
+        if isinstance(herbivore, Herbivore) and not herbivore.hidden:
             herbivore.health -= 50
             herbivore.die()
