1f46be77543978b3fb122231c672682c2f021fa1
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-01-04 16:55:37 +0200

    fix annotation and indentaion

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from __future__ import annotations
 
 
 class Animal:
@@ -12,8 +12,8 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}, Health: {self.health}, " +\
-            f"Hidden: {self.hidden}}}"
+        return (f"{{Name: {self.name}, Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
@@ -22,7 +22,7 @@ class Herbivore(Animal):
 
 
 class Carnivore(Animal):
-    def bite(self, other: Callable) -> None:
+    def bite(self, other: Carnivore) -> None:
         if not isinstance(other, Carnivore) and not other.hidden:
             other.health -= 50
             if other.health <= 0:
