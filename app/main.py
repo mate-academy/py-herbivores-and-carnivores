f24d3244afc9116563331701aff234765c9d9407
PythonZem <pythonzem@gmail.com>
2024-01-15 10:13:23 +0200

    fix code style

--- app/main.py
+++ app/main.py
@@ -1,19 +1,23 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str,
-                 health: int = 100,
-                 hidden: bool = False
-                 ) -> None:
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
         self.name = name
         self.hidden = hidden
         self.health = health
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name}" \
-               f", Health: {self.health}" \
-               f", Hidden: {self.hidden}}}"
+        return (
+            f"{{Name: {self.name},"
+            f" Health: {self.health},"
+            f" Hidden: {self.hidden}}}"
+        )
 
 
 class Herbivore(Animal):
