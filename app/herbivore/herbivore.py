253bc3901b1112a7c7270cc18fe456a145aacc9f
D Petetskyi <4477617@gmail.com>
2024-01-16 08:18:59 +0200

    Arranged all modules to main.py

--- app/herbivore/herbivore.py
+++
@@ -1,9 +0,0 @@
-from ..animal.animal import Animal
-
-
-class Herbivore(Animal):
-    def hide(self) -> None:
-        if self.hidden:
-            self.hidden = False
-        else:
-            self.hidden = True
--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
-from app.animal.animal import Animal
-from app.herbivore.herbivore import Herbivore
-from app.carnivore.carnivore import Carnivore
+class Animal:
+    alive = list()
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = False
+        self.alive.append(self)
+
+    def __repr__(self) -> str:
+        return ("{" + f"Name: {self.name},"
+                f" Health: {self.health}, Hidden: {self.hidden}" + "}")
 
 
-print(Animal.__dict__)
-print(Herbivore.__dict__)
-print(Carnivore.__dict__)
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, victim: Herbivore) -> None:
+        if isinstance(victim, Herbivore) and victim.hidden is False:
+            victim.health -= 50
+            if victim.health < 1:
+                Animal.alive.remove(victim)
