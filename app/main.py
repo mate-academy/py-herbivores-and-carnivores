e89559f910bef85ee4adcbe4e4a33e83bd1f442e
voidmain69 <photoreznichenko@gmail.com>
2024-01-28 14:09:40 +0200

    fix __repr__ method class Animal

--- app/main.py
+++ app/main.py
@@ -14,10 +14,13 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"" \
-               f"{{Name: {self.name}," \
-               f" Health: {self.health}," \
-               f" Hidden: {self.hidden}}}"
+        return (
+            f"{{"
+            f"Name: {self.name},"
+            f" Health: {self.health},"
+            f" Hidden: {self.hidden}"
+            f"}}"
+        )
 
 
 class Herbivore(Animal):
