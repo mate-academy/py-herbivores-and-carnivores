d7214df8e2f5f9f9e9db1f10cf96e5219d932e61
Liudmyla <4888814@gmail.com>
2024-02-01 13:36:24 +0200

    'solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,36 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = False
+
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        class_dict = {
+            "Name": self.name,
+            "Health": self.health,
+            "Hidden": self.hidden
+        }
+        rep = (f"{{"
+               f"Name: {class_dict["Name"]}, "
+               f"Health: {class_dict["Health"]}, "
+               f"Hidden: {class_dict["Hidden"]}"
+               f"}}")
+        return rep
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, whom: Herbivore) -> None:
+        if (not whom.hidden) and isinstance(whom, Herbivore):
+            whom.health -= 50
+
+        if whom.health <= 0:
+            Animal.alive.remove(whom)
