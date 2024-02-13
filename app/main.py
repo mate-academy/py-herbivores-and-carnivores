2f40273597b9f54713e03e82ea3056c2f889eb2a
Mikola Rozhansky <bymikola@gmail.com>
2024-02-13 23:48:20 +0200

    Temp_1

--- app/main.py
+++ app/main.py
@@ -16,7 +16,17 @@ class Herbivore(Animal):
     def hide(self):
         self.hidden = not self.hidden
 
+
 class Carnivore(Animal):
+
+    def bite(self, herb_inst: Herbivore) -> Herbivore:
+        if not (herb_inst.hidden and isinstance(herb_inst, Carnivore)):
+            herb_inst.health -= 50
+            return herb_inst
+
+
+
+
 
 
 
