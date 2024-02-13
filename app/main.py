d45baa5dea6a7f0af09e79e97274ebdb371f40e5
Mikola Rozhansky <bymikola@gmail.com>
2024-02-13 23:36:40 +0200

    Temp_1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,27 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self,
+                 name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
+        self.health = health
+        self.name = name
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+
+class Herbivore(Animal):
+
+    def hide(self):
+        self.hidden = not self.hidden
+
+class Carnivore(Animal):
+
+
+
+
+
+
+
+
