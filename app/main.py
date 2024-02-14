a719e315a9d8a61219ae47b7733c24872fc4d935
Oleksandr <gubar3113@gmail.com>
2024-02-14 21:49:18 +0200

    HerbCarn_Solution3

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ class Herbivore(Animal):
 
 class Carnivore(Animal):
     @staticmethod
-    def bite(target: str) -> None:
+    def bite(target: Herbivore) -> None:
         if isinstance(target, Herbivore) and not target.hidden:
             target.health -= 50
             if target.health <= 0:
