41f7425040a04ee8063a02b8e6f72208906ba73e
meier <Meyertal2@gmail.com>
2024-02-25 17:48:25 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+class Animal:
+    def __init__(self, name: str, health: int) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = False
+        self.alive = True if self.health > 0 else False
+
+class Herbivore(Animal):
+    def hide(self):
+        self.hidden = True
+
