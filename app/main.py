f42512cf446e74cdccaaee724e1b579f0d89ba16
Mikita Havryliuk <chinter.games1@gmail.com>
2024-02-25 13:24:45 +0200

    feature: solved task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,40 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(
+        self,
+        name: str,
+        health: int = 100,
+        hidden: bool = False
+    ) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __str__(self) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def die(self) -> None:
+        Animal.alive.remove(self)
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    @staticmethod
+    def bite(animal: Animal) -> None:
+        if not isinstance(animal, Carnivore) and not animal.hidden:
+            animal.health -= 50
+        if animal.health <= 0:
+            animal.die()
