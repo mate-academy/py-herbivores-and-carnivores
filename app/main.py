9c0355084a891ef4ac829c0c7be56ae8c31d4c23
AndriiDraga <148384465+AndriiDraga@users.noreply.github.com>
2024-02-25 19:18:30 +0200

    'fixed'

--- app/main.py
+++ app/main.py
@@ -8,14 +8,15 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return \
-            (f"{{Name: {self.name}, "
-             f"Health: {self.health}, "
-             f"Hidden: {self.hidden}}}")
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
     @classmethod
-    def remove_dead(cls) -> None:
-        cls.alive = [a for a in cls.alive if a.health > 0]
+    def remove_dead(cls, dead_animal: "Animal") -> None:
+        cls.alive = [animal for animal in cls.alive if animal != dead_animal]
 
 
 class Herbivore(Animal):
@@ -25,7 +26,7 @@ class Herbivore(Animal):
 
 class Carnivore(Animal):
     @staticmethod
-    def bite(herbivore: "Herbivore") -> None:
+    def bite(herbivore: "Animal") -> None:
         if not isinstance(herbivore, Carnivore) and not herbivore.hidden:
             herbivore.health -= 50
-            Animal.remove_dead()
+            Animal.remove_dead(herbivore)
