5d14dcbef72f9d55413fe5b5982d60183e2aaa31
antonver <anton.verbovyi@gmail.com>
2024-03-09 15:37:47 +0100

    Revert "yes"
    This reverts commit 8e376919a17c1382d3e7f993cd5b8b39d99b4243.

--- app/main.py
+++ app/main.py
@@ -23,8 +23,7 @@ class Herbivore(Animal):
 
 
 class Carnivore(Animal):
-    @staticmethod
-    def bite(animal: Herbivore) -> None:
+    def bite(self, animal: Herbivore) -> None:
         if isinstance(animal, Herbivore) and not animal.hidden:
             animal.health -= 50
             if animal.health <= 0:
