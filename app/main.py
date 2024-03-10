5425c9a51b3f74bc080ddf78d5a4c72e0355f959
Andrii Zubchenko <dissom1987@gmail.com>
2024-03-10 22:35:53 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ from __future__ import annotations
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health: int = 100) -> None:
+    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
         self.name = name
         self.health = health
-        self.hidden = False
+        self.hidden = hidden
         Animal.alive.append(self)
 
     def die(self) -> None:
@@ -29,7 +29,7 @@ class Herbivore(Animal):
 
 class Carnivore(Animal):
     @staticmethod
-    def bite(target: str) -> None:
+    def bite(target: Herbivore) -> None:
         if isinstance(target, Herbivore) and not target.hidden:
             target.health -= 50
         target.die()
@@ -40,5 +40,21 @@ if __name__ == "__main__":
     lion = Carnivore("Simba")
     lion2 = Carnivore("King Lion")
 
+    lion = Carnivore("Lion King")
+    rabbit = Herbivore("Susan")
+    print(rabbit.health)
+    lion.bite(rabbit)
+    print(rabbit.health)  # bited
+
+    # rabbit.hide()
+    rabbit.hide()
+    lion.bite(rabbit)
+    print(rabbit.health)  # lion cannot bite hidden rabbit
+
+    rabbit.hide()
+    lion.bite(rabbit)
+    print(rabbit.health) == 0  # rabbit is dead
+
+    print(rabbit in Animal.alive)  # False
     for animal in Animal.alive:
         print(animal)
