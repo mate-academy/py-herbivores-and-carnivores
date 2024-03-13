6e5e4817b871c6ee1010dd231ddbb795a38a44eb
vova-stoliar <vova.stoliar@overdose.digital>
2024-03-13 18:56:37 +0200

    Fix errors

--- app/main.py
+++ app/main.py
@@ -4,15 +4,20 @@ from __future__ import annotations
 class Animal:
     alive: list[Animal] = []
 
-    def __init__(self, name: str, health: int = 100, hidden: bool = False):
+    def __init__(self,
+                 name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
 
         Animal.alive.append(self)
 
-    def __repr__(self):
-        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
