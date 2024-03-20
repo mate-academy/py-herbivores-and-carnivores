2de8d4d83023068201f5545ad1dfafef3a257f4d
Dede <specialized8393@gmail.com>
2024-03-20 13:40:31 +0000

    () instead of \ on 11 line

--- app/main.py
+++ app/main.py
@@ -8,11 +8,11 @@ class Animal:
         self.__class__.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{" \
-               f"Name: {self.name}, " \
-               f"Health: {self.health}, " \
-               f"Hidden: {self.hidden}" \
-               f"}}"
+        return (f"{{"
+                f"Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}"
+                f"}}")
 
 
 class Herbivore(Animal):
