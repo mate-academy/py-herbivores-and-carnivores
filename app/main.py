287d4ee0c7d6d60b3b756974a80fc869f6286b3d
Sofiia <sonagm9@gmail.com>
2024-03-22 02:25:28 +0200

    'Solution1_fix_flake8'

--- app/main.py
+++ app/main.py
@@ -1,31 +1,36 @@
+from typing import Union
+
+
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health=100, hidden=False) -> None:
+    def __init__(self, name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
         Animal.alive.append(self)
 
-    def die(self):
+    def die(self) -> None:
         if self.health <= 0:
             Animal.alive.remove(self)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return (f"{{Name: {self.name}, "
                 f"Health: {self.health}, "
                 f"Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
-    def hide(self):
+    def hide(self) -> None:
         self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
 
     @staticmethod
-    def bite(pray):
+    def bite(pray: Union[Herbivore, Animal]) -> None:
         if isinstance(pray, Herbivore) and pray.hidden is False:
             pray.health -= 50
             pray.die()
