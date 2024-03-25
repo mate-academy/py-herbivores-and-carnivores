e5e9851723201588f51f8636c8c46e565199f8e2
semermaks <semermaks@mail.com>
2024-03-25 09:32:05 +0200

    Use class name for accessing class attrs

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ class Animal:
         self.health = health
         self.name = name
         self.hidden = hidden
-        self.alive.append(self)
+        Animal.alive.append(self)
 
     def die(self) -> None:
         Animal.alive.remove(self)
