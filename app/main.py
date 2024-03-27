22ae2202c4156f3b677d52e617a40844d8b52498
Ira <iramladanovych@gmail.com>
2024-03-27 11:18:30 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,41 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, health=100, name="", hidden=False):
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __repr__(self):
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+
+    def decrease_health(self, amount: int):
+        self.health -= amount
+        if self.health <= 0:
+            self.health = 0
+            self.die()
+
+    def die(self):
+        Animal.alive.remove(self)
+
+
+class Herbivore(Animal):
+    def __init__(self, name):
+        super().__init__(name=name)
+
+    def hide(self):
+        self.hidden = not self.hidden
+class Carnivore(Animal):
+    def __init__(self, name):
+        super().__init__(name=name)
+
+    @staticmethod
+    def bite(animal):
+        if isinstance(animal, Herbivore) and not animal.hidden:
+            animal.decrease_health(50)
+
+
+pantera = Carnivore("Bagira")
+snake = Carnivore("Kaa")
+print(Animal.alive)
