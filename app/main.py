0154a11c7c3ba518dc10a2c4e59f16819b7655fd
Ira <iramladanovych@gmail.com>
2024-03-27 12:43:39 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,38 +1,50 @@
-class Animal:
-    alive = []
+from typing import List
 
-    def __init__(self, health=100, name="", hidden=False):
+
+class Animal:
+    alive: List["Animal"] = []
+
+    def __init__(
+        self, health: int = 100, name: str = "", hidden: bool = False
+    ) -> None:
+
         self.name = name
         self.health = health
         self.hidden = hidden
         Animal.alive.append(self)
 
-    def __repr__(self):
-        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+    def __repr__(self) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
-    def decrease_health(self, amount: int):
+    def decrease_health(self, amount: int) -> None:
         self.health -= amount
         if self.health <= 0:
             self.health = 0
             self.die()
 
-    def die(self):
+    def die(self) -> None:
         Animal.alive.remove(self)
 
 
 class Herbivore(Animal):
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         super().__init__(name=name)
 
-    def hide(self):
+    def hide(self) -> None:
         self.hidden = not self.hidden
+
+
 class Carnivore(Animal):
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         super().__init__(name=name)
 
     @staticmethod
-    def bite(animal):
-        if isinstance(animal, Herbivore) and not animal.hidden:
+    def bite(animal: "Herbivore") -> None:
+        if not animal.hidden:
             animal.decrease_health(50)
 
 
