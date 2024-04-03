2d4350ad7c5b78192261d2f763fab22da88441de
Nazar <legoman1858@gmail.com>
2024-04-03 23:41:17 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,29 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.health = health
+        self.name = name
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}"
+                )
+
+
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    @staticmethod
+    def bite(enemy: Herbivore) -> None:
+        if isinstance(enemy, Herbivore) and not enemy.hidden:
+            enemy.health = enemy.health - 50
+            if enemy.health <= 0:
+                Animal.alive.remove(enemy)
