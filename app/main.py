2e89262aa76c9f25722999f7ef82081d2044d2d1
Olena Poplias <poplyasitasony@gmail.com>
2024-04-16 15:48:04 -0400

    "Hide simplify"

--- app/main.py
+++ app/main.py
@@ -23,10 +23,7 @@ class Animal:
 
 class Herbivore(Animal):
     def hide(self) -> None:
-        if not self.hidden:
-            self.hidden = True
-        else:
-            self.hidden = False
+        self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
