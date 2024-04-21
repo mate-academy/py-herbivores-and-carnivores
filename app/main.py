3aad7285906f72879a8b29072dbe901c07eea5ef
unknown <ruslan.b201520@gmail.com>
2024-04-21 09:40:08 +0300

    flake8 fixes

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health: int = 100, hidden: bool = False):
-        self.name=name
-        self.health=health
-        self.hidden=False
+    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = False
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return f"{{Name: {self.name: str}, Health: {self.health: int}, Hidden: {self.hidden: bool}}}"
+        return f"{{Name: {self.name: str}, 
+                  Health: {self.health: int}, Hidden: {self.hidden: bool}}}"
 
 
 class Herbivore(Animal):
