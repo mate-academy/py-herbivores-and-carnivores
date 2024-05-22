d63bdd1a91ad7a4c83c9e335d82376b022c6618f
Vova <v.kampanchenko@gmail.com>
2024-05-22 22:19:53 +0300

    Solution1

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
+    def __init__(
+            self, name: str, health: int = 100, hidden: bool = False
+    ) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
         Animal.alive.append(self)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
 
 
