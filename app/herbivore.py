6d620dc658a034cb143b525cd3f0fb4816d816a3
Maksym Protsak <maksym.protsak@gmail.com>
2024-05-26 10:56:39 +0300

    Solution

--- app/herbivore.py
+++
@@ -1,7 +0,0 @@
-from app.animal import Animal
-
-
-class Herbivore(Animal):
-
-    def hide(self) -> None:
-        self.hidden = not self.hidden
--- app/main.py
+++ app/main.py
@@ -1,3 +1,32 @@
-from app.animal import Animal
-from app.herbivore import Herbivore
-from app.carnivore import Carnivore
+from __future__ import annotations
+
+
+class Animal:
+    alive = []
+    health = 100
+    hidden = False
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.name = name
+        self.alive.append(self)
+        self.health = health
+
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
+
+
+class Carnivore(Animal):
+
+    def bite(self, herbivore: Herbivore) -> None:
+        if isinstance(herbivore, Herbivore) and not herbivore.hidden:
+            herbivore.health = herbivore.health - 50
+        if herbivore.health <= 0:
+            Animal.alive.remove(herbivore)
+
+
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+        self.hidden = not self.hidden
