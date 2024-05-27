577f94d1428e5896081982ba0cd3ef390c2917a1
unlie9 <unlie.devops9@gmail.com>
2024-05-27 14:32:37 +0300

    solution1

--- app/main.py
+++ app/main.py
@@ -1,26 +1,24 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health=100) -> None:
+    def __init__(self, name: str, health: int = 100) -> None:
         self.name = name
         self.health = health
         self.hidden = False
         Animal.alive.append(self)
 
-    def die(self):
+    def die(self) -> None:
         if self in Animal.alive:
             Animal.alive.remove(self)
 
-    def __repr__(self) -> None:
-        info = {}
-        result = []
-
-        for animal in Animal.alive:
-            info["Name"] = animal.name,
-            info["Health"] = animal.health,
-            info["Hidden"] = animal.hidden
-
-        return info
+    def __repr__(self) -> str:
+        return (
+            f"{{"
+            f"Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}"
+            f"}}"
+        )
 
 
 class Herbivore(Animal):
