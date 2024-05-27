971c6dbfe8edfc5452f97780fccaf12fec88e59e
unlie9 <unlie.devops9@gmail.com>
2024-05-27 13:50:42 +0300

    3 failed 9 passed

--- app/main.py
+++ app/main.py
@@ -1,1 +1,31 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.health = 100
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def die(self):
+        if self in Animal.alive:
+            Animal.alive.remove(self)
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    @staticmethod
+    def bite(herbivore: Herbivore) -> None:
+        if herbivore.hidden is False:
+            if herbivore.health == 100:
+                herbivore.health = 50
+            if herbivore.health == 50:
+                Animal.die(herbivore)
+
+        if herbivore.hidden is True:
+            herbivore.health = 100
+
