4b67af9ce817422ed7a160304f20eab0e6024ccc
K. Petrychenko <catpetrychenko@gmail.com>
2024-05-31 00:50:01 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,33 @@
-# write your code here
+from typing import List
+
+
+class Animal:
+    alive: List["Animal"] = []
+
+    def __init__(self, name: str, health: int = 100,
+                 hidden: bool = False) -> None:
+        self.name: str = name
+        self.health: int = health
+        self.hidden: bool = hidden
+        self.__class__.alive.append(self)
+
+    def __str__(self) -> str:
+        return f"{{Name: {self.name}, " \
+            f"Health: {self.health}, Hidden: {self.hidden}}}"
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, target: Animal) -> None:
+        if isinstance(target, Carnivore) or target.hidden:
+            return
+        target.health -= 50
+        if target.health <= 0:
+            self.__class__.alive.remove(target)
