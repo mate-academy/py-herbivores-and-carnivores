493cb0d4cbd66633dbd3e6eecd4a715b2ce6c7c7
Halyna Rohatska <rohatskahr@gmail.com>
2024-06-20 21:47:28 +0300

    implemented Predator and Herbivore classes that inherit from the Animal class

--- app/main.py
+++ app/main.py
@@ -1,1 +1,37 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def die(self) -> None:
+        if self in Animal.alive:
+            Animal.alive.remove(self)
+
+    def __repr__(self) -> str:
+        return (f"{{"
+                f"Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}"
+                f"}}")
+
+    def __str__(self) -> None:
+        print(Animal.alive.__repr__())
+
+
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    @staticmethod
+    def bite(victim: Animal) -> None:
+        if isinstance(victim, Herbivore) and victim.hidden is False:
+            victim.health -= 50
+        if victim.health <= 0:
+            victim.die()
