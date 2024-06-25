5cb4e488c92b70c45a9189df5fc3c7fd5168bd64
ulichka <ulichka7615@gmail.com>
2024-06-25 13:12:26 +0300

    fixed the bugs

--- app/main.py
+++ app/main.py
@@ -23,17 +23,14 @@ class Animal:
 class Herbivore(Animal):
 
     def hide(self) -> None:
-        if self.hidden is True:
-            self.hidden = False
-        else:
-            self.hidden = True
+        self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
 
     @staticmethod
-    def bite(other: Animal) -> None:
-        if isinstance(other, Herbivore) and other.hidden is False:
+    def bite(other: Herbivore) -> None:
+        if isinstance(other, Herbivore) and not other.hidden:
             if other.health <= 50:
                 Animal.alive.remove(other)
             else:
