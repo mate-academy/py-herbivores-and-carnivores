fb85330f6efe7c1fe776ed652b21e6066cf13d7f
maksym_korniev <maksymkorniev88@gmail.com>
2024-06-28 00:13:45 +0300

    successfully implement OOP realisation of animals with hide and bite methods

--- app/main.py
+++ app/main.py
@@ -1,1 +1,33 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100,
+                 hidden: bool = False) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, Hidden: {self.hidden}}}")
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        if self.hidden:
+            self.hidden = False
+        else:
+            self.hidden = True
+
+
+class Carnivore(Animal):
+    def bite(self, herbivore: Herbivore) -> None:
+        if not isinstance(herbivore, Herbivore):
+            return
+
+        if not herbivore.hidden:
+            herbivore.health -= 50
+
+        if herbivore.health <= 0:
+            super().alive.remove(herbivore)
