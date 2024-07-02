63be0d292d08146d0db23bf78442e5d17a85bc21
Illia <illiatsiuper@gmail.com>
2024-07-02 12:42:04 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,45 @@
-# write your code here
+from typing import Callable
+
+
+class Animal:
+    alive = []
+
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def take_damage(self, amount: int) -> Callable:
+        self.health -= amount
+        if self.health <= 0:
+            self.die()
+
+    def die(self) -> Callable:
+        self.health = 0
+        if self in Animal.alive:
+            Animal.alive.remove(self)
+
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, Hidden: {self.hidden}}}")
+
+    @classmethod
+    def __str__(cls) -> str:
+        return str([str(animal) for animal in cls.alive])
+
+
+class Herbivore(Animal):
+    def hide(self) -> Callable:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, herbivore: Herbivore) -> int:
+        if isinstance(herbivore, Herbivore) and not herbivore.hidden:
+            herbivore.take_damage(50)
