449de23f6596c007eded38efc1e26bc35f485ec0
unknown <kykyangelina182@gmail.com>
2024-07-05 18:32:31 +0300

    olution

--- app/main.py
+++ app/main.py
@@ -22,13 +22,7 @@ class Herbivore(Animal):
 
 class Carnivore(Animal):
     def bite(self, animal: Animal) -> None:
-        if isinstance(animal, Carnivore):
-            return
-
-        if animal.hidden:
-            return
-
-        animal.health -= 50
-
-        if animal.health <= 0:
-            Animal.alive.remove(animal)
+        if isinstance(animal, Herbivore) and not animal.hidden:
+            animal.health -= 50
+            if animal.health <= 0:
+                Animal.alive.remove(animal)
