808bf2d4fbd03ed7a6766bfdacbe36fea5560a27
DSahalatyi <dsahalatyi@gmail.com>
2024-07-21 00:32:55 +0300

    fix class parameters

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ class Herbivore(Animal):
 
 class Carnivore(Animal):
     @staticmethod
-    def bite(prey: Herbivore) -> None:
+    def bite(prey: Animal) -> None:
         if isinstance(prey, Herbivore) and not prey.hidden:
             prey.health -= 50
             if prey.health <= 0:
