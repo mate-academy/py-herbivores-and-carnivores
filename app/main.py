fe682f74d858122b7f82290e7c8a68f62a65fb5a
Marharyta Zazulia <zazmargo@gmail.com>
2024-07-25 17:39:18 -0300

    fixed __repr__

--- app/main.py
+++ app/main.py
@@ -11,9 +11,8 @@ class Animal:
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        result = ""
-        result += "{" + (f"Name: {self.name}, Health: {self.health}, "
-                         + f"Hidden: {self.hidden}") + "}"
+        result = "{" + (f"Name: {self.name}, Health: {self.health}, "
+                        f"Hidden: {self.hidden}") + "}"
         return result
 
 
