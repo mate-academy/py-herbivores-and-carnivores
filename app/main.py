18b3682de24a1709832d5a2c8ce79dbbaf2712aa
Vladyslav Bondarenko <128479458+VladyslavBon@users.noreply.github.com>
2024-09-30 18:13:45 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,28 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return str(
+            {key.capitalize(): value for key, value in self.__dict__.items()}
+        )
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = True if not self.hidden else False
+
+
+class Carnivore(Animal):
+    @classmethod
+    def bite(cls, herbivore: Herbivore) -> None:
+        if isinstance(herbivore, Herbivore):
+            if not herbivore.hidden:
+                herbivore.health -= 50
+            if herbivore.health <= 0:
+                cls.alive.remove(herbivore)
