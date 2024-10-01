4aa0ee2b2516a5304e8a5f28a782bb2c94d27298
roman <skyfoxwork@gmail.com>
2024-10-01 12:06:16 +0300

    Solution-py-herbivores-and-carnivores v1.0

--- app/main.py
+++ app/main.py
@@ -12,8 +12,12 @@ class Animal:
         self.hidden = hidden
         Animal.alive.append(self)
 
-    def __repr__(self):
-        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+    def __repr__(self) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
 
 class Herbivore(Animal):
@@ -32,21 +36,3 @@ class Carnivore(Animal):
 
         if animal.health <= 0:
             Animal.alive.remove(animal)
-
-
-# lion = Carnivore("Lion King")
-# rabbit = Herbivore("Susan")
-# print(rabbit.health == 100)
-# lion.bite(rabbit)
-# print(rabbit.health == 50) # bited
-#
-# rabbit.hide()
-# lion.bite(rabbit)
-# print(rabbit.health == 50)  # lion cannot bite hidden rabbit
-#
-# rabbit.hide()
-# lion.bite(rabbit)
-# print(rabbit.health == 0)  # rabbit is dead
-#
-# print(Animal.alive)
-# print(Animal)
