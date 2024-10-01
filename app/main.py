274d6314ad791131e24a2c365a1bd4eb848809a3
Vitalii Mykytenko <vitalii.mykytenko@gmail.com>
2024-10-01 23:39:16 +0300

    create parent class «Animal»

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
-# write your code here
+class Animal:
+
+    alive = []
+
+    def __init__(self,
+                 name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
+
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        self.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (
+            f"{{Name: {self.name},"
+            f"Health: {self.health},"
+            f"Hidden: {self.hidden}}}"
+        )
+
+    def die(self) -> None:
+        if self.health <= 0:
+            self.alive.remove(self)
