8f33d9474518cd8bddcde5f066f41e36c8ae4ab7
yuliiapyrih <yuliia.pyrih.pm.2018@lpnu.ua>
2024-10-01 22:57:39 +0300

    implement py-herbivores-and-carnivores

--- app/main.py
+++ app/main.py
@@ -1,1 +1,43 @@
-# write your code here
+from __future__ import annotations
+
+
+class Animal:
+    alive = []
+
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
+
+    @staticmethod
+    def print_alive() -> str:
+        return f"[{", ".join([repr(animal) for animal in Animal.alive])}]"
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+    def die(self) -> None:
+        Animal.alive.remove(self)
+
+
+class Carnivore(Animal):
+    @staticmethod
+    def bite(herbivore: Herbivore) -> None:
+        if isinstance(herbivore, Herbivore) and not herbivore.hidden:
+            herbivore.health -= 50
+
+            if herbivore.health <= 0:
+                herbivore.die()
