eb700c69e5f6caade20467f67479ca1a70dd1206
Maksym Kaptsov <makskaptsov13@gmail.com>
2024-10-03 16:03:29 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,35 @@
-# write your code here
+from typing import List, Union
+
+
+class Animal:
+    alive: List["Animal"] = []
+
+    def __init__(self, name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def die(self) -> None:
+        if self.health <= 0:
+            Animal.alive.remove(self)
+
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, herbivore: Union[Herbivore, "Carnivore"]) -> None:
+        if isinstance(herbivore, Herbivore) and not herbivore.hidden:
+            herbivore.health -= 50
+            if herbivore.health <= 0:
+                herbivore.die()
