5ed24854adde51ab38c768cb723b0024432decd8
Max Savenya <savenya2013@gmail.com>
2024-10-06 13:05:09 +0300

    Solution updated v2

--- app/main.py
+++ app/main.py
@@ -19,16 +19,9 @@ class Animal:
             Animal.alive.remove(self)
 
     def __repr__(self) -> str:
-        return (
-            f"{{"
-            f"Name: {self.name}, "
-            f"Health: {self.health}, "
-            f"Hidden: {self.hidden}}}"
-        )
-
-    @classmethod
-    def alive_list_repr(cls) -> str:
-        return f"[{", ".join([repr(animal) for animal in cls.alive])}]"
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
