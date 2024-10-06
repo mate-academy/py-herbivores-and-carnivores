81085cee95128ab845eca66d584874e807fadd88
Max Savenya <savenya2013@gmail.com>
2024-10-06 12:49:03 +0300

    Solution updated

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
+    def __init__(
+        self, name: str, health: int = 100, hidden: bool = False
+    ) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
@@ -17,10 +19,12 @@ class Animal:
             Animal.alive.remove(self)
 
     def __repr__(self) -> str:
-        return (f"{{"
-                f"Name: {self.name}, "
-                f"Health: {self.health}, "
-                f"Hidden: {self.hidden}}}")
+        return (
+            f"{{"
+            f"Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
     @classmethod
     def alive_list_repr(cls) -> str:
