d7f2b62843fbbb0ba0bd7cc6f12e20bd40f0af4e
Oleksandr <rigby8806@gmail.com>
2024-10-09 21:41:24 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -13,8 +13,9 @@ class Animal:
             Animal.alive.remove(self)
 
     def __repr__(self) -> str:
-        return (f"{{Name: {self.name}, "
-                f"Health: {self.health}, Hidden: {self.hidden}}}")
+        return (f'{{Name: {self.name}, '
+                f'Health: {self.health}, '
+                f'Hidden: {self.hidden}}}')  # Use double quotes and f-string
 
 
 class Herbivore(Animal):
