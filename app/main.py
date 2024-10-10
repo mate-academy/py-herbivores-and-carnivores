a423a080bd94489d2f2f441aa30deeae7ce06558
Oleksii Ivanov <oleks4335@gmail.com>
2024-10-10 21:58:31 +0200

    Add herbivores and carnivores solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,40 @@
-# write your code here
+from __future__ import annotations
+
+
+class Animal:
+    alive: list = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def is_alive(self) -> bool:
+        return self.health > 0
+
+    @staticmethod
+    def remove_dead(animal: Herbivore) -> None:
+        if not animal.is_alive():
+            Animal.alive.remove(animal)
+
+    def __repr__(self) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
+
+
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+
+    def bite(self, herbivore: Herbivore) -> None:
+        if isinstance(herbivore, Herbivore) and not herbivore.hidden:
+            herbivore.health -= 50
+            Animal.remove_dead(herbivore)
